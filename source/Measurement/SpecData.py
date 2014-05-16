d557a6ff503f4fe35396c25d0d23bcca8a3502c3
Michael Hammen <hammen@uni-mainz.de>
2014-05-16 14:16:26 +0200

    Finished revamp and arithmetic scalers. Order is now swapped with self.cts[track][scaler][step] of type list(2d-ndarray).

--- source/Measurement/SpecData.py
+++ source/Measurement/SpecData.py
@@ -3,6 +3,7 @@ Created on 29.03.2014
 
 @author: hammen
 '''
+import itertools as it
 
 import numpy as np
 
@@ -15,38 +16,51 @@ class SpecData(object):
         '''
         Constructor
         '''
+        self.path = None
+        self.type = None
+        self.line = None
         self.time = 0
         self.nrLoops = [1]
         self.nrTracks = 1
         self.nrScalers = 1
-        self.type = None
-        self.line = None
-        self.path = None
-        #self.x = np.array()
-        #self.cts = np.array()
-        #self.err = np.array()
         self.accVolt = None
         self.laser = None
         self.col = None
+        
+        #Data is organized as list of tracks containing arrays with information
+        #self.x = [np.array((steps,))]
+        #self.cts = [np.array((scaler, steps))]
+        #self.err = [np.array((scaler, steps))]
         
         
     def getSingleSpec(self, scaler, track):
         '''Return a tuple with (volt, cts, err) of the specified scaler and track. -1 for all tracks'''        
         if track == -1:
-            return (self.x.flatten(), self.cts[scaler].flatten(), self.err[scaler].flatten())
+            return ( [i for i in it.chain(*self.x)], [i for i in it.chain(*(t[scaler] for t in self.cts))], [i for i in it.chain(*(t[scaler] for t in self.err))] )
         else:
-            return (self.x[track], self.cts[scaler][track], self.err[scaler][track])
+            return (self.x[track], self.cts[track][scaler], self.err[track][scaler])
+    
     
     def getArithSpec(self, scaler, track):
         '''Same as getSingleSpec, but scaler is of type [+i, -j, +k], resulting in s[i]-s[j]+s[k]'''
-        aCts = np.zeros((self.nrTracks,))
+        l = self.getNrSteps(track)
+        flatc = np.zeros((l,))
+        flate = np.zeros((l,))
         
-        for i in scaler:
-            for i, t in enumerate(self.cts[abs(i)]):
-                
-                tCts = t * np.copysign(1, i)
+        for s in scaler:
+            flatx, c, e = self.getSingleSpec(abs(s), track)
+            flatc = flatc + np.copysign(1, s) * c
+            flate = flate + np.square(e)
             
+        flate = np.sqrt(flate)
         
+        return (flatx, flatc, flate)
+        
+    def getNrSteps(self, track):
+        if track == -1:
+            return sum(map(len, self.x))
+        else:
+            return len(self.x[track])
 
     def _normalizeTracks(self):
         '''Check whether a different number of loops was used for the different tracks and correct'''
