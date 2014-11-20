dd9ac8ad74279aa40b93805ab0dad37c39ebb02c
skaufmann <simkaufm@uni-mainz.de>
2014-11-20 11:22:26 +0100

    created shared library project and first test via math file

---
+++ mymath.c
@@ -0,0 +1,12 @@
+/*
+ * mymath.c
+ *
+ *  Created on: 20.11.2014
+ *      Author: noertert
+ */
+
+#include "mymath.h"
+
+int mult(int a, int b){
+	return a * b;
+}
---
+++ mymath.h
@@ -0,0 +1,13 @@
+/*
+ * mymath.h
+ *
+ *  Created on: 20.11.2014
+ *      Author: noertert
+ */
+
+#ifndef MYMATH_H_
+#define MYMATH_H_
+
+int mult(int a, int b);
+
+#endif /* MYMATH_H_ */
