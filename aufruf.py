dfbcabfd5b93e573486bddfb10d5550e092b8300
skaufmann <simkaufm@uni-mainz.de>
2015-01-20 19:14:32 +0100

    Values are corectly read out, but appendign the values to the Queue does not work yet.

--- aufruf.py
+++ aufruf.py
@@ -8,29 +8,64 @@ import time
 
 dll = ctypes.CDLL('D:\Workspace\Eclipse\Tilda\TildaTarget\SimpleCounter\SimpleCounter.dll')
 
-# print(dll.mult(7,2))
-# print(dll.add(1,3))
-# test2 = (ctypes.c_int*3)()
-# print(ctypes.byref(test2))
-# dll.blub(ctypes.byref(test2))
-# print(test2[0])
-# 
-dmaCts = (ctypes.c_int*2)()
-print(ctypes.byref(dmaCts))
+
+
+
 status = dll._init()
 print('Status: ' + str(status))
 session = dll._openFPGA()
 print('aktuelle Session: ' + str(session))
-# rate = dll._rate(session, 100)
+ 
 status = dll._runFPGA(session)
 print('running FPGA, Status is:' + str(status))
-status = dll._stop(session)
-print('Loop stopped, Status is:' + str(status))
 time.sleep(0.045)
 nOfEle = dll.DMAnOfEle(session)
 print('Number of Elements in the Queue: ' + str(nOfEle))
-# dll.readDMA(session, nOfele, ctypes.byref(dmaCts))
 
+# Problem, da groesse vom Array festgelegt werden muss!?
+
+dmaCts = (ctypes.c_int * nOfEle)()
+print(dmaCts)
+print(ctypes.byref(dmaCts))
+
+
+dll.readDMA(session, nOfEle, ctypes.byref(dmaCts))
+time.sleep(0.045)
+status = dll._stop(session)
+print('Loop stopped, Status is:' + str(status))
+nOfEle = dll.DMAnOfEle(session)
+print('Number of Elements still in the Queue: ' + str(nOfEle))
+
+# dll.readDMA(session, nOfEle, ctypes.byref(dmaCts))
+print('here we go')
+print(dmaCts)
+print(dmaCts[0], '{0:032b}'.format(dmaCts[0]))
+print(dmaCts[1], '{0:032b}'.format(dmaCts[1]))
+print(dmaCts[2], '{0:032b}'.format(dmaCts[2]))
+print(dmaCts[15], '{0:032b}'.format(dmaCts[15]))
+
+dll.readDMA(session, nOfEle, ctypes.byref(dmaCts))
+
+print('here we go again')
+print(dmaCts)
+print(dmaCts[0], '{0:032b}'.format(dmaCts[0]))
+print(dmaCts[1], '{0:032b}'.format(dmaCts[1]))
+print(dmaCts[2], '{0:032b}'.format(dmaCts[2]))
+print(dmaCts[15], '{0:032b}'.format(dmaCts[15]))
+
+
+# while True:
+
+#     try:
+# #         print(bin(dmaCts[0:nOfEle]))
+#         for i in range(0, nOfEle, 1):
+#             print(bin(dmaCts[i]))
+# #             print('PMT: ' + str(bin(dmaCts[i])[2:5]) + '\t cts/20ms: ' + str(bin(dmaCts[i])[7:]))
+#         else: 
+#             True  
+#     except:
+#         True
+#            
 status = dll._fpgaexit(session)
 
 print('Exiting Program, Status: ' + str(status))
