62eddd873d442812624a95e2f1ddc0f2438d64df
skaufmann <simkaufm@uni-mainz.de>
2015-01-20 16:13:40 +0100

    number of Elements in DMA is correctly working :)

--- aufruf.py
+++ aufruf.py
@@ -4,24 +4,37 @@ Created on 08.08.2014
 @author: simkaufm
 '''
 import ctypes
+import time
 
-dll = ctypes.CDLL('D:\Workspace\Eclipse\Tilda\TildaTarget\Debug\libTildaTarget.dll')
+dll = ctypes.CDLL('D:\Workspace\Eclipse\Tilda\TildaTarget\SimpleCounter\SimpleCounter.dll')
 
-print(dll.mult(7,2))
-print(dll.add(1,3))
+# print(dll.mult(7,2))
+# print(dll.add(1,3))
 # test2 = (ctypes.c_int*3)()
 # print(ctypes.byref(test2))
 # dll.blub(ctypes.byref(test2))
 # print(test2[0])
 # 
-# dmaCts = (ctypes.c_int*2)()
-# print(ctypes.byref(dmaCts))
-# status = dll._init()
-# print('Status: ' + str(status))
-# session = dll._openFPGA()
-# print('aktuelle Session: ' + str(session))
+dmaCts = (ctypes.c_int*2)()
+print(ctypes.byref(dmaCts))
+status = dll._init()
+print('Status: ' + str(status))
+session = dll._openFPGA()
+print('aktuelle Session: ' + str(session))
 # rate = dll._rate(session, 100)
-# status = dll._runFPGA(session)
+status = dll._runFPGA(session)
+print('running FPGA, Status is:' + str(status))
+status = dll._stop(session)
+print('Loop stopped, Status is:' + str(status))
+time.sleep(0.045)
+nOfEle = dll.DMAnOfEle(session)
+print('Number of Elements in the Queue: ' + str(nOfEle))
+# dll.readDMA(session, nOfele, ctypes.byref(dmaCts))
+
+status = dll._fpgaexit(session)
+
+print('Exiting Program, Status: ' + str(status))
+
 # print('Repititionsrate auf  ' + str(rate) +' ms gesetzt, Running ... Status: '+str(status))
 #  
 # # print(dll._checkTemp(session))
@@ -59,6 +72,5 @@ print(dll.add(1,3))
 #     except:
 #         True
 #  
-# status = dll._fpgaexit(session)
-# print('Exiting Program, Status: ' + str(status))
+
 
