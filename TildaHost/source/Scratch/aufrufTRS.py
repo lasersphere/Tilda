ca241b684804d091020cc5c387f8bbf4457ed2f6
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-05-08 17:48:33 +0200

    chnaged something in the aufrufTRS.py so now one can call the functions determined in the NiFpga.c/.h files directly, not having to use the TRSwrapper.c commands

--- TildaHost/source/Scratch/aufrufTRS.py
+++ TildaHost/source/Scratch/aufrufTRS.py
@@ -15,11 +15,34 @@ import ctypes
 
 dll = ctypes.CDLL('D:\Workspace\Eclipse\Tilda\TildaHost\\binary\TRS.dll')
 
-status = dll.init()
-print('Status: ' + str(status))
-session = dll.openFPGA()
-print('aktuelle Session: ' + str(session))
+'''
+first using TRWwrapper.c commands
+'''
 
-status = dll.runFPGA(session)
-print('running FPGA, Status is:' + str(status))
+# 
+# status = dll.init()
+# print('Status: ' + str(status))
+# session = dll.openFPGA()
+# print('aktuelle Session: ' + str(session))
+#  
+# status = dll.runFPGA(session)
+# print('running FPGA, Status is:' + str(status))
+ 
 
+
+'''
+now just using the standard NiFpga.c/.h Commands!
+'''
+
+status = dll.NiFpga_Initialize()
+print(status) 
+dllPath = ctypes.create_string_buffer(b'D:\\Workspace\\Eclipse\\Tilda\\TildaTarget\\bin\\TimeResolvedSequencer\\NiFpga_TRS.lvbitx')
+dllSign = ctypes.create_string_buffer(b'BF31570369009FA00617B7055FD697C8')
+resource = ctypes.create_string_buffer(b'Rio1')
+session = ctypes.c_ulong()
+print(session)
+status = dll.NiFpga_Open(dllPath, dllSign, resource, 1, ctypes.byref(session))
+print(status)
+print(session)
+status = dll.NiFpga_Run(ctypes.byref(session), 0)
+print(status)
