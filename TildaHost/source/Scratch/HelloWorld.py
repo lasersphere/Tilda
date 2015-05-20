05dc8ad7959a1980db23a194b68309921809d208
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-05-20 13:50:08 +0200

    minor changes in helloWorld
    began to draw dataflow in uml but not finished

--- TildaHost/source/Scratch/HelloWorld.py
+++ TildaHost/source/Scratch/HelloWorld.py
@@ -3,18 +3,25 @@ Created on 20.11.2014
 
 @author: noertert
 '''
-import ctypes
-print('Hello World!')
+# import ctypes
+import numpy as np
+# print('Hello World!')
+#
+# DACQuWriteTimeout = {'ref': 0x8116, 'val': ctypes.c_bool(), 'ctr': False}
+#
+# def func(dict):
+#     print(dict['val'].value)
+#     print(dict['ref'])
+#
+# func(DACQuWriteTimeout)
 
-DACQuWriteTimeout = {'ref': 0x8116, 'val': ctypes.c_bool(), 'ctr': False}
+#
+# print(len((ctypes.c_ulong * 0)()))
+# print(int('00000010000000000000', 2))
+# print(int('00000000000001000000', 2))
 
-def func(dict):
-    print(dict['val'].value)
-    print(dict['ref'])
-    
-func(DACQuWriteTimeout)
-
-
-print(len((ctypes.c_ulong * 0)()))
-print(int('00000010000000000000', 2))
-print(int('00000000000001000000', 2))
+nOfBins = 100
+nOfSteps = 20
+testmatrix = np.zeros((nOfBins, nOfSteps, 8), dtype=np.int)
+print(testmatrix)
+print(type(testmatrix[0, 0, 0]))
