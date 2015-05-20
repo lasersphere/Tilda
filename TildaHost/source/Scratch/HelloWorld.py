a3eebe20db57dcede5f4e675956dd0d6e4c05e12
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-05-20 14:02:46 +0200

    added old stuff in helloWorld, just as a messy copy

--- TildaHost/source/Scratch/HelloWorld.py
+++ TildaHost/source/Scratch/HelloWorld.py
@@ -25,3 +25,38 @@ nOfSteps = 20
 testmatrix = np.zeros((nOfBins, nOfSteps, 8), dtype=np.int)
 print(testmatrix)
 print(type(testmatrix[0, 0, 0]))
+
+
+#old main stuff:
+#
+# from Driver.DataAcquisitionFpga.TimeResolvedSequencer import TimeResolvedSequencer
+# from Service.Formating import Formatter
+# import time
+#
+# class Main():
+#     def __init__(self):
+#         self.trs = TimeResolvedSequencer()
+#         self.form = Formatter()
+#         self.fullData = []
+#
+#     def measureOneTrack(self, scanpars):
+#         self.trs.measureTrack(scanpars)
+#         while self.trs.getSeqState() == self.trs.TrsCfg.seqState['measureTrack']:
+#             result = self.trs.getData()
+#             if result['nOfEle'] == 0:
+#                 break
+#             else:
+#                 print(result)
+#                 newData = [self.form.integerSplitHeaderInfo(result['newData'][i]) for i in range(len(result['newData']))]
+#                 print(newData)
+#                 self.fullData.append(newData)
+#                 time.sleep(0.4)
+#         print(self.fullData)
+#
+#
+#
+# maininst = Main()
+# print(maininst.measureOneTrack(maininst.trs.TrsCfg.dummyScanParameters))
+#
+# # print(format(814743552, '032b'))
+# # print(len(bin(814743552)[2:]))
