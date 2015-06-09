6bc7c983b5bf22868261a3b8d2d3d3a9700612cb
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-06-09 14:39:09 +0200

    changed xmlTest in order to load xml Data. Loading is not working smoothly yet.

--- TildaHost/source/Service/Tests/xmlTests.py
+++ TildaHost/source/Service/Tests/xmlTests.py
@@ -23,12 +23,12 @@ exampleVoltArray = np.random.randint(10, size=20)
 exampleScalerArray = np.random.randint(100, size=(20, 2000, 8))
 exampleTimeArray = np.random.randint(0, 10, size=2000)
 
-# form.xmlAddDataToTrack(bodyRoot, 0, 'scalerArray', np.array_str(exampleScalerArray))
-# form.xmlAddDataToTrack(bodyRoot, 0, 'voltArray', np.array_str(exampleVoltArray))
-# form.xmlAddDataToTrack(bodyRoot, 0, 'timeArray', np.array_str(exampleTimeArray))
-# handl.saveXml(bodyRoot, 'DummyData.xml')
+#form.xmlAddDataToTrack(bodyRoot, 0, 'scalerArray', np.array_str(exampleScalerArray))
+#form.xmlAddDataToTrack(bodyRoot, 0, 'voltArray', np.array_str(exampleVoltArray))
+#form.xmlAddDataToTrack(bodyRoot, 0, 'timeArray', np.array_str(exampleTimeArray))
+#handl.saveXml(bodyRoot, 'DummyData.xml')
 
 loadedText = handl.loadXml('DummyData.xml').find('tracks').find('track0').find('scalerArray').text
 loadedText = loadedText.replace('\\n','')
-# loadedTextnp = np.fromstring(loadedText, dtype=)
-# print(loadedTextnp)
+#loadedTextnp = np.fromstring(loadedText)
+print(loadedText)
