879a16831361bea546d77d1206fcb77e3623c4e0
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-06-30 16:41:55 +0200

    Started writing TestModule for Heinzinger

---
+++ TildaHost/source/Service/Tests/HeinzingerTest.py
@@ -0,0 +1,12 @@
+"""
+
+Created on '30.06.2015'
+
+@author:'simkaufm'
+
+"""
+
+import Driver.Heinzinger.Heinzinger as hz
+import Driver.Heinzinger.HeinzingerCfg as hzCfg
+
+heinz = hz.Heinzinger(hzCfg.comportHeinzinger0)
