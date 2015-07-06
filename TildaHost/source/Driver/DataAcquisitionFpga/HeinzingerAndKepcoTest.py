869fa99d67474d247fffacbec1437bb87704c773
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-07-06 17:40:33 +0200

    filled HeinzingerAndKepcoTest.py with code. should work like this.

--- TildaHost/source/Driver/DataAcquisitionFpga/HeinzingerAndKepcoTest.py
+++ TildaHost/source/Driver/DataAcquisitionFpga/HeinzingerAndKepcoTest.py
@@ -1,1 +1,51 @@
 __author__ = 'noertert'
+
+
+from Driver.DataAcquisitionFpga.FPGAInterfaceHandling import FPGAInterfaceHandling
+import Driver.DataAcquisitionFpga.HeinzingerAndKepcoTestConfig as hat
+
+class HsbAndDac(FPGAInterfaceHandling):
+
+    def __init__(self):
+        self.setDacReg = 0
+        self.actDacReg = 0
+        self.fpgaInst = super(HsbAndDac, self).__init__(hat.bitfilePath, hat.bitfileSignature,
+                                                        hat.fpgaResource)
+
+# '''read indicators'''
+    def readDacState(self):
+        return self.ReadWrite(hat.DacState).value
+    def readActDacReg(self):
+        return self.ReadWrite(hat.actDACRegister).value
+
+# '''write controls'''
+    def setHsb(self, deviceStr):
+        """
+        function to control which Heinzinger/Kepco is set by the Heinzinger Switch Box (HSB)
+        :param deviceStr: str, naming the desired device
+        :return: fpga state
+        """
+        deviceNumber = hat.hsbDict.get(deviceStr)
+        if deviceNumber != None:
+            self.fpgaInst.ReadWrite(hat.heinzingerControl, deviceNumber)
+        return self.checkFpgaStatus()
+
+    def setDacState(self, stateStr):
+        """
+        function to set the state of the DAC
+        :param stateStr: str, naming the desired state
+        :return: fpga state
+        """
+        stateNumber = hat.dacStatesDict.get(stateStr)
+        if stateNumber != None:
+            self.fpgaInst.ReadWrite(hat.DacStateCmdByHost, stateNumber)
+        return self.checkFpgaStatus()
+
+    def setDacRegister(self, regVal):
+        """
+        function to set the value of the DAC register
+        :param regVal: int
+        :return: fpga state
+        """
+        self.ReadWrite(hat.setDACRegister, regVal)
+        return self.checkFpgaStatus()
--- TildaHost/source/Driver/DataAcquisitionFpga/HeinzingerAndKepcoTestConfig.py
+++ TildaHost/source/Driver/DataAcquisitionFpga/HeinzingerAndKepcoTestConfig.py
@@ -1,10 +1,11 @@
 __author__ = 'noertert'
 
+import ctypes
 
 '''Bitfile Signature:'''
 bitfileSignature = '65983986BDA85857BB806DE8DBC8A8B6'
 '''Bitfile Path:'''
-bitfilePath = 'D:\Workspace\PyCharm\Tilda\TildaTarget\bin\TimeResolvedSequencer\NiFpga_HeinzingerAndKepcoTest.lvbitx'
+bitfilePath = 'D:\\Workspace\\PyCharm\\Tilda\\TildaTarget\\bin\\TimeResolvedSequencer\\NiFpga_HeinzingerAndKepcoTest.lvbitx'
 '''FPGA Resource:'''
 fpgaResource = 'Rio1'
 '''Indicators:'''
@@ -15,3 +16,8 @@ heinzingerControl = {'ref': 0x811E, 'val': ctypes.c_ubyte(), 'ctr': True}
 DacStateCmdByHost = {'ref': 0x811A, 'val': ctypes.c_uint(), 'ctr': True}
 setDACRegister = {'ref': 0x810C, 'val': ctypes.c_ulong(), 'ctr': True}
 '''TargetToHostFifos:'''
+
+
+'''Zustandsnummerierung Labview'''
+hsbDict = {'Kepco': 0, 'Heinzinger0': 1, 'Heinzinger1': 2, 'Heinzinger2': 3}
+dacStatesDict = {'init': 0, 'idle': 1, 'setVolt': 2, 'error': 3}
