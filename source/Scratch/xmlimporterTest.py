6d0d8de36853dae8c0b6629f30a4aa895bb6b5d3
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-08-18 15:34:28 +0200

    working towards v1.06 and plotting the v1.04 data

--- source/Scratch/xmlimporterTest.py
+++ source/Scratch/xmlimporterTest.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 from Measurement.XMLImporter import XMLImporter
 import MPLPlotter
 
-path = 'D:/Workspace/PyCharm/Tilda/PolliFit/test/Project/Data/testTilda.xml'
+# path = 'D:/Workspace/PyCharm/Tilda/PolliFit/test/Project/Data/testTilda.xml'
+path = 'C:\\Workspace\\TildaTestData\\PulserOfflineTests_150806\\sortedByRuns\\run0\\cs_sum_Nothing_000.xml'
 file = XMLImporter(path)
 # MPLPlotter.plot(file.x[0], file.cts[0])
 # MPLPlotter.show()
