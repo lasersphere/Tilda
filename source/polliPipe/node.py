2080582ed1b93d4a40a6661f46c3c2456d12f54a
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-08-25 10:25:40 +0200

    failed on branching in pipeline but dont want to waste more time on this task. Therefore for now the pipeline and its plotting is only linear possible.

--- source/polliPipe/node.py
+++ source/polliPipe/node.py
@@ -47,7 +47,7 @@ class Node(object):
             newData = self.processData(item.data, self.Pipeline.pipeData)
             if newData is not None:
                 item.data = newData
-                # newjobs = self.createJobs(item, False)
+                # newjobs = self.createJobs(item, False)  # like this, pyqtgraph allows branching in pipeline
                 newjobs = self.createJobs(item)  # original line, but pyqtgraph does not like this.
 
         return newjobs
