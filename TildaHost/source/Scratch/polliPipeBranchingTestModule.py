f9db260b05fbec5f13bfb46ae7ba4553b21b03c8
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-08-27 16:39:17 +0200

    working testmodule for Michael

--- TildaHost/source/Scratch/polliPipeBranchingTestModule.py
+++ TildaHost/source/Scratch/polliPipeBranchingTestModule.py
@@ -10,7 +10,19 @@ Currently there is a problem with the references to the figure
 """
 
 import Service.AnalysisAndDataHandling.tildaPipeline as TP
+import Service.FolderAndFileHandling as fileHandl
+import matplotlib.pyplot as plt
 
+import os
+
+# set paths for test data
+rawfiles = [os.path.join('exampleData\\raw', file) for file in os.listdir('exampleData\\raw') if file.endswith('.raw')]
+file = 'exampleData\\sums\\cs_sum_Ca_40_000.xml'
+workdir = 'C:\\TestData'
+# configure scan dictionary
+scnd = fileHandl.loadPickle('exampleData\\raw\\cs_Ca_40_track0_000.pipedat')
+scnd['pipeInternals']['activeXmlFilePath'] = None
+scnd['pipeInternals']['filePath'] = workdir
 
 
 def TestPipe():
@@ -18,6 +30,34 @@ def TestPipe():
     start = TP.Node()
 
     pipe = TP.Pipeline(start)
+    pipe.pipeData = scnd
 
-    TP.initPipeData()
+    fig, axes = plt.subplots(3, sharex=True)
 
+    walk = start.attach(TP.TN.NSplit32bData())
+    walk = walk.attach(TP.TN.NSortRawDatatoArray(pipe.pipeData))
+
+    # branching works for now by using shallow copy, if the NotImplementedError occures
+    # this is realized by a try loop in processItem in node.py
+    # if this is removed and deepcopy is used, the pipeline will fail
+    branch = walk.attach(TP.TN.NAccumulateSingleScan(pipe.pipeData))
+
+    branch1 = branch.attach(TP.TN.NArithmetricScaler([0]))
+    branch1 = branch1.attach(TP.TN.NMPlLivePlot(pipe.pipeData, axes[0], 'single Scan scaler 0'))
+
+    branch2 = branch.attach(TP.TN.NArithmetricScaler([1]))
+    branch2 = branch2.attach(TP.TN.NMPlLivePlot(pipe.pipeData, axes[1], 'single Scan scaler 1'))
+
+    walk = walk.attach(TP.TN.NRemoveTrackCompleteFlag())
+    walk = walk.attach(TP.TN.NSumCS(pipe.pipeData))
+
+    walk = walk.attach(TP.TN.NMPlLivePlot(pipe.pipeData, axes[2], 'live sum'))
+
+    return pipe
+
+
+pipe = TestPipe()
+pipe.start()
+
+for f in rawfiles:
+    pipe.feed(fileHandl.loadPickle(f))
