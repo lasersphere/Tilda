d494a829c352593deb872d41b6060c6065cf59c5
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-10-26 12:55:35 +0100

    moved all lxml operations from Formating.py to XmlOperations.py

--- source/Measurement/XMLImporter.py
+++ source/Measurement/XMLImporter.py
@@ -15,6 +15,7 @@ import ast
 import numpy as np
 
 from Measurement.SpecData import SpecData
+from Service.FileFormat.XmlOperations import xmlGetDataFromTrack
 import Service.FolderAndFileHandling as tildaFileHandl
 import Service.Formating as tildaForm
 
@@ -62,7 +63,7 @@ class XMLImporter(SpecData):
             dacStepSize18Bit = val['dacStepSize18Bit']
             dacStop18Bit = dacStart18Bit + (dacStepSize18Bit * nOfsteps)
             xAxis = np.arange(dacStart18Bit, dacStop18Bit, dacStepSize18Bit)
-            ctsstr = tildaForm.xmlGetDataFromTrack(lxmlEtree, nOfactTrack, 'scalerArray')
+            ctsstr = xmlGetDataFromTrack(lxmlEtree, nOfactTrack, 'scalerArray')
             cts = tildaForm.numpyArrayFromString(ctsstr, (nOfsteps, nOfScalers))
             self.nrScalers.append(nOfScalers)
             self.x.append(xAxis)
