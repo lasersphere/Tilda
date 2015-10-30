1517c2732a4bb0f20213521a709ee32456faefd1
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-10-30 13:24:24 +0100

    database is now created when a working directory has been set.

--- source/Tools.py
+++ source/Tools.py
@@ -155,7 +155,7 @@ def createDB(db):
     shape TEXT,
     fixShape TEXT,
     charge INT,
-    FOREIGN KEY (reference) REFERENCES Isotopes (iso)
+    FOREIGN KEY (reference) REFERENCES Isotopes (iso),
     FOREIGN KEY (refRun) REFERENCES Runs (run)
     )''')
     
@@ -213,7 +213,7 @@ def createDB(db):
     statErrForm TEXT DEFAULT err,
     systErr FLOAT,
     systErrForm TEXT DEFAULT 0,
-    PRIMARY KEY (iso, parname, run)
+    PRIMARY KEY (iso, parname, run),
     FOREIGN KEY (run) REFERENCES Runs (run)
     )''')
 
