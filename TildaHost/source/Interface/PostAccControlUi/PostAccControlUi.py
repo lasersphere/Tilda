c63dc32a9e6a1bda09f83cae504474f309a6707b
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-11-19 17:25:04 +0100

    fixed wrong naming

--- TildaHost/source/Interface/PostAccControlUi/PostAccControlUi.py
+++ TildaHost/source/Interface/PostAccControlUi/PostAccControlUi.py
@@ -35,10 +35,11 @@ class PostAccControlUi(QtWidgets.QMainWindow, Ui_MainWindow_PostAcc):
 
     def init_pow_sups(self):
         self.scan_main.init_post_accel_pwr_supplies()
-        print(self.scan_main.post_accel_pwr_supplies.active_power_supplies)
+        self.update_power_sups_gui()
 
     def update_power_sups_gui(self):
         act_dict = self.post_acc_main.active_power_supplies
+        print('dictionary is: ', act_dict)
         for name, instance in act_dict:
             status_dict = self.post_acc_main.get_status_of_pwr_supply()
             self.update_single_power_sup(status_dict, name[-1:])
