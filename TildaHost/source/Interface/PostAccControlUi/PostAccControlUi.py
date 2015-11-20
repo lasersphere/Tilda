fc8588592b082f0435fa5f808578853e67376f9e
skaufmann <simkaufm@uni-mainz.de>
2015-11-20 11:07:16 +0100

    Gui for controling the Heinzinger works fine. (more tests still ongoing but looks promising)

--- TildaHost/source/Interface/PostAccControlUi/PostAccControlUi.py
+++ TildaHost/source/Interface/PostAccControlUi/PostAccControlUi.py
@@ -7,6 +7,7 @@ Module Description:
 """
 
 from PyQt5 import QtWidgets
+import logging
 from copy import copy
 from Interface.PostAccControlUi.Ui_PostAccControl import Ui_MainWindow_PostAcc
 
@@ -21,24 +22,33 @@ class PostAccControlUi(QtWidgets.QMainWindow, Ui_MainWindow_PostAcc):
         self.post_acc_main = main.scan_main.post_acc_main
 
         self.update_power_sups_gui()
+        # self.connect_buttons()
         # self.label_name1.setText('Test')
         # self.label_con1.text()
         # self.doubleSpinBox_set_volt1.value()
-        for i in range(1, 4):
-            getattr(self, 'pushButton_set_volt' + str(i)).connect(self.volt_set(*self.resol_power_sup(i)))
-            getattr(self, 'pushButton_on_off' + str(i)).connect(self.set_outp(*self.resol_power_sup(i)))
         # self.label_last_set1
         # self.label_volt_read1
         # self.pushButton_on_off1
         self.pushButton_init_all.clicked.connect(self.init_pow_sups)
         self.pushButton_refresh.clicked.connect(self.update_power_sups_gui)
-        # self.pushButton_all_on_off
+        self.pushButton_all_on_off.clicked.connect(self.all_off)
 
         self.show()
+
+    def connect_buttons(self):
+        for i in range(1, 4):
+            try:
+                name = self.resol_power_sup(i)
+                getattr(self, 'pushButton_set_volt' + str(i)).clicked.connect(getattr(self, 'set_volt' + str(i)))
+                getattr(self, 'pushButton_on_off' + str(i)).clicked.connect(getattr(self, 'set_outp' + str(i)))
+            except Exception as e:
+                logging.error('while connecting the buttons for ' + name + ' the following exception occurred: ' +
+                              ' \n\n' + str(e))
 
     def init_pow_sups(self):
         self.scan_main.init_post_accel_pwr_supplies()
         self.update_power_sups_gui()
+        self.connect_buttons()
 
     def update_power_sups_gui(self):
         act_dict = self.post_acc_main.active_power_supplies
@@ -52,23 +62,56 @@ class PostAccControlUi(QtWidgets.QMainWindow, Ui_MainWindow_PostAcc):
         getattr(self, 'label_last_set' + num_str).setText(status_dict.get('voltageSetTime'))
         getattr(self, 'label_volt_read' + num_str).setText(str(status_dict.get('readBackVolt')))
         getattr(self, 'label_volt_read' + num_str).setText(str(status_dict.get('readBackVolt')))
-        self.button_color(getattr(self, 'pushButton_on_off' + num_str), status_dict.get('output'))
+        self.button_color('pushButton_on_off' + num_str, status_dict.get('output'))
 
     def resol_power_sup(self, number):
         name = getattr(self, 'label_name' + str(number)).text()
-        voltage = getattr(self, 'doubleSpinBox_set_volt' + str(number)).value()
-        return name, voltage
+        return name
 
-    def volt_set(self, name, voltage):
+    def volt_set(self, name):
         if name is not None:
-            self.scan_main.set_Voltage(name, voltage)
+            voltage = getattr(self, 'doubleSpinBox_set_volt' + name[-1:]).value()
+            if name is not None:
+                self.scan_main.set_post_accel_pwr_supply(name, voltage)
+                self.update_power_sups_gui()
+
+    def set_volt1(self):
+        self.volt_set(self.resol_power_sup(1))
+
+    def set_volt2(self):
+        self.volt_set(self.resol_power_sup(2))
+
+    def set_volt3(self):
+        self.volt_set(self.resol_power_sup(3))
+
+    def set_outp(self, name, outp=None):
+        if name is not None:
+            status_dict = self.scan_main.get_status_of_pwr_supply(name)
+            cur_outp = status_dict.get('output', False)
+            if outp is None:
+                new_outp = not cur_outp  # if not specified, just toggle the output status
+            else:
+                new_outp = outp
+            self.scan_main.set_post_accel_pwr_spply_output(name, new_outp)
             self.update_power_sups_gui()
 
-    def set_outp(self, name, voltage):
-        status_dict = self.scan_main.get_status_of_pwr_supply(name)
-        cur_outp = status_dict.get('output', False)
-        self.scan_main.set_post_accel_pwr_spply_output(name, not cur_outp)
-        self.update_power_sups_gui()
+    def set_outp1(self):
+        self.set_outp(self.resol_power_sup(1))
+
+    def set_outp2(self):
+        self.set_outp(self.resol_power_sup(2))
+
+    def set_outp3(self):
+        self.set_outp(self.resol_power_sup(3))
+
+    def all_off(self):
+        for i in range(1, 3):
+            try:
+                name = self.resol_power_sup(i)
+                self.set_outp(name, outp=False)
+            except Exception as e:
+                logging.error('While setting all outputs off the following error occurred: \n\n ' +
+                              str(e) + '\n \n')
 
     def button_color(self, butn, on_off):
         if on_off:
