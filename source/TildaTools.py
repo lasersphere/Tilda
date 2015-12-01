3c3b268e183403607fada22a7ecdc829ea5cf8e7
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2015-12-01 10:03:44 +0100

    moved all functions necessary for importing an xml file to Pollifit.

---
+++ source/TildaTools.py
@@ -0,0 +1,137 @@
+"""
+Created on 
+
+@author: simkaufm
+
+Module Description: Tools related closely to Tilda
+"""
+import ast
+import os
+
+import numpy as np
+from lxml import etree as ET
+
+
+def merge_dicts(d1, d2):
+    """ given two dicts, merge them into a new dict as a shallow copy """
+    new = d1.copy()
+    new.update(d2)
+    return new
+
+
+def numpy_array_from_string(string, shape):
+    """
+    converts a text array saved in an lxml.etree.Element
+    using the function xmlWriteToTrack back into a numpy array
+    :param string: str, array
+    :param shape: int, or tuple of int, the shape of the output array
+    :return: numpy array containing the desired values
+    """
+    string = string.replace('\\n', '').replace('[', '').replace(']', '').replace('  ', ' ')
+    result = np.fromstring(string, dtype=np.uint32, sep=' ')
+    result = result.reshape(shape)
+    return result
+
+
+def get_track_names(scan_dict):
+    """
+    create a list of all track names in the scan dictionary, sorted by its tracknumber
+    :return: ['track0', 'track1', ...]
+    """
+    track_list = [key for key, val in scan_dict.items() if 'track' in str(key)]
+    return sorted(track_list)
+
+
+def eval_str_vals_in_dict(dicti):
+    """
+    function to convert the values of a dictionary to int, float or list, if it is possible
+    """
+    for key, val in dicti.items():
+        try:
+            dicti[key] = ast.literal_eval(val)
+        except Exception as e:
+            print(e, val, type(val))
+    return dicti
+
+
+def load_xml(filename):
+    """
+    loads an .xml file and returns it as an lxml.etree.Element
+    :return:lxml.etree.Element, Element of loaded File
+    """
+    tree = ET.parse(filename)
+    elem = tree.getroot()
+    return elem
+
+
+def save_xml(root_Ele, path, pretty=True):
+    """
+    Convert a Root lxml Element into an ElementTree and save it to a file
+    """
+    np.set_printoptions(threshold=np.nan)
+    tree = ET.ElementTree(root_Ele)
+    tree.write(path, pretty_print=pretty)
+
+
+def xml_get_dict_from_ele(element):
+    """
+    Converts an lxml Element into a python dictionary
+    """
+    return element.tag, dict(map(xml_get_dict_from_ele, element)) or element.text
+
+
+def get_all_tracks_of_xml_in_one_dict(xml_file):
+    """
+    reads from an xml file and returns all tracks with its parameters
+    :return: dict, {'track0': {...}, 'track1':{...}, ...}
+    """
+    xml_etree = load_xml(xml_file)
+    trackd = {}
+    tracks = xml_etree.find('tracks')
+    for t in tracks:
+        trackd[str(t.tag)] = (xml_get_dict_from_ele(xml_etree)[1]['tracks'][str(t.tag)]['header'])
+    for key, val in trackd.items():
+        trackd[key] = eval_str_vals_in_dict(trackd[key])
+    return trackd
+
+def xml_get_data_from_track(root_ele, n_of_track, data_type):
+    """
+    Get Data From Track
+    :param root_ele:  lxml.etree.Element, root of the xml tree
+    :param n_of_track: int, which Track should be written to
+    :param data_type: str, valid: 'setOffset, 'measuredOffset', 'dwellTime10ns', 'nOfmeasuredSteps',
+     'nOfclompetedLoops', 'voltArray', 'timeArray', 'scalerArray'
+    :param returnType: int or tuple of int, shape of the numpy array, 0 if output in textfrom is desired
+    :return: Text
+    """
+    try:
+        actTrack = root_ele.find('tracks').find('track' + str(n_of_track)).find('data')
+        dataText = actTrack.find(str(data_type)).text
+        return dataText
+    except:
+        print('error while searching ' + str(data_type) + ' in track' + str(n_of_track) + ' in ' + str(root_ele))
+        return None
+
+
+def scan_dict_from_xml_file(xml_file_name, scan_dict=None):
+    """
+    creates a Scandictionary for the standard Tilda operations
+    values are gained from the loaded xmlFile
+    :return: dict, xmlEtree, Scandictionary gained from the xml file, and xmlEtree element.
+    """
+    if scan_dict is None:
+        scan_dict = {}
+    xml_etree = load_xml(xml_file_name)
+    trackdict = get_all_tracks_of_xml_in_one_dict(xml_file_name)
+    scan_dict = merge_dicts(scan_dict, trackdict)
+    isotopedict = xml_get_dict_from_ele(xml_etree)[1]['header']
+    for key, val in isotopedict.items():
+        if key in ['accVolt', 'laserFreq', 'nOfTracks']:
+            isotopedict[key] = ast.literal_eval(val)
+    scan_dict['isotopeData'] = isotopedict
+    scan_dict['pipeInternals'] = {}
+    scan_dict['pipeInternals']['workingDirectory'] = os.path.split(os.path.split(xml_file_name)[0])[0]
+    scan_dict['pipeInternals']['curVoltInd'] = 0
+    scan_dict['pipeInternals']['activeTrackNumber'] = 'None'
+    scan_dict['pipeInternals']['activeXmlFilePath'] = xml_file_name
+    return scan_dict, xml_etree
