e3d7888bdf2f55530ced9aeac73f97d012aa907d
Simon Kaufmann <s.kaufmann@uni-mainz.de>
2016-03-03 09:09:19 +0100

    kepco settlement time analysis BM, Tag 1

--- PolliFit/source/Analysis/TildaOnlineCaAnalysis.py
+++ PolliFit/source/Analysis/TildaOnlineCaAnalysis.py
@@ -35,11 +35,17 @@ databases:
 db = 'R:\Projekte\TRIGA\Measurements and Analysis_Simon\Bunchermessungen2016' + \
      '\CalciumOnline_160223_analysis\CalciumOnline_160223.sqlite'
 
+
 """ folder """
 work_dir = os.path.split(db)[0]
 combine_plots_dir = os.path.join(work_dir, 'combined_plots')
 if not os.path.exists(combine_plots_dir):
     os.makedirs(combine_plots_dir)
+
+pick_file = os.path.join(combine_plots_dir, 'plot_data.dat')
+isos = ['40_Ca', '40_Ca_broad']
+pars = ['center', 'sigma', 'Int0']
+runs = ['sc0', 'sc1', 'sc0+sc1']
 
 '''crawl'''
 # Tools.crawl(db)
@@ -52,102 +58,86 @@ if not os.path.exists(combine_plots_dir):
 # print(f.cts)
 # print(f.dwell)
 
-# ''' cs fits: '''
-# # fit = InteractiveFit('cs_40_Ca_broad_000.xml', db, 'sc0', block=False)
-# # fit.fit()
-# files = Tools.fileList(db, '40_Ca_broad')
-# dirty_files = ['cs_40_Ca_broad_001.xml', 'cs_40_Ca_broad_002.xml',
-#                '40_Ca_broad_cs_000.xml', '40_Ca_broad_cs_002.xml', '40_Ca_broad_cs_003.xml',
-#                '40_Ca_broad_cs_004.xml', '40_Ca_broad_cs_005.xml']
-# files = [file for file in files if file not in dirty_files]
-# cs_file = []
-# for f in files:
-#     try:
-#         meas = XMLImporter(os.path.join(os.path.split(db)[0], 'sums', f))
-#         if meas.seq_type == 'cs':
-#             cs_file.append(f)
-#     except Exception as e:
-#         print(e)
-# print(cs_file)
-# ''' batch fitting '''
-# runs = ['sc0', 'sc1', 'sc0+sc1']
-#
-# for run in runs:
-#     BatchFit.batchFit(cs_file, db, run)
+''' fitting all files for the kepco settle time related stuff: '''
 
-# ''' trs fits: '''
-# fit = InteractiveFit('trs_40_Ca_001.xml', db, 'sc1', block=False)
+#
+files = ['cs_40_Ca_broad_000.xml', 'cs_40_Ca_broad_003.xml',
+         'cs_40_Ca_broad_006.xml', 'cs_40_Ca_broad_007.xml',
+         'trs_40_Ca_001.xml', 'trs_40_Ca_002.xml', 'trs_40_Ca_003.xml',
+         'trs_40_Ca_004.xml', 'trs_40_Ca_005.xml', 'trs_40_Ca_006.xml',
+         'trs_40_Ca_007.xml', 'trs_40_Ca_008.xml', 'trs_40_Ca_011.xml'
+         ]
+# files = ['cs_40_Ca_broad_003.xml',
+#          'trs_40_Ca_001.xml', 'trs_40_Ca_002.xml', 'trs_40_Ca_003.xml'
+#          ]
+# files = [files[-1]]
+# fit = InteractiveFit(files[-3], db, 'sc0+sc1', block=True)
 # fit.fit()
-files = Tools.fileList(db, '40_Ca')
-dirty_files = ['trs_40_Ca_000.xml', 'trs_40_Ca_009.xml', 'trs_40_Ca_010.xml',
-               '40_Ca_trs_000.xml', '40_Ca_trs_001.xml', '40_Ca_trs_002.xml',
-               '40_Ca_trs_006.xml']
-files = [file for file in files if file not in dirty_files]
-files = [file for file in files if '_proj' not in file]
-trs_file = []
-for f in files:
-    try:
-        meas = XMLImporter(os.path.join(os.path.split(db)[0], 'sums', f))
-        if meas.seq_type == 'trs':
-            trs_file.append(f)
-    except Exception as e:
-        print(e)
-print(trs_file)
+
 ''' batch fitting '''
-runs = ['sc0', 'sc1', 'sc0+sc1']
 
-for run in runs:
-    BatchFit.batchFit(trs_file, db, run)
-
-
-# ''' Average '''
-# pick_file = os.path.join(combine_plots_dir, 'plot_data.dat')
-#
-# pl = {}
 # for run in runs:
-#     pl[run] = {'center':{}, 'sigma':{}, 'Int0':{}}
-#     avgc, statErrc, systErrc, plotdatac = Analyzer.combineRes('40_Ca_broad', 'center', run, db, show_plot=True)
-#     avgs, statErrs, systErrs, plotdatas = Analyzer.combineRes('40_Ca_broad', 'sigma', run, db, show_plot=False)
-#     avgi, statErri, systErri, plotdatai = Analyzer.combineRes('40_Ca_broad', 'Int0', run, db, show_plot=False)
-#     pl[run]['center'] = plotdatac
-#     pl[run]['sigma'] = plotdatas
-#     pl[run]['Int0'] = plotdatai
-# pickle.dump(pl, open(pick_file, "wb"))  # saving the plot data in order nto to connect to the db everytime
+#     BatchFit.batchFit(files, db, run)
+
+
+
+''' Average '''
+
+pl = dict.fromkeys(isos,  dict.fromkeys(runs, dict.fromkeys(pars, {})))
+for iso in isos:
+    pl[iso] = dict.fromkeys(runs, {})
+    for run in runs:
+        # pl[iso][run] = {'center': {}, 'sigma': {}, 'Int0': {}}
+        pl[iso][run] = {}
+        avgc, statErrc, systErrc, plotdatac = Analyzer.combineRes(iso, 'center', run, db, show_plot=False)
+        avgs, statErrs, systErrs, plotdatas = Analyzer.combineRes(iso, 'sigma', run, db, show_plot=False)
+        avgi, statErri, systErri, plotdatai = Analyzer.combineRes(iso, 'Int0', run, db, show_plot=False)
+        pl[iso][run]['center'] = deepcopy(plotdatac)
+        pl[iso][run]['sigma'] = deepcopy(plotdatas)
+        pl[iso][run]['Int0'] = deepcopy(plotdatai)
+
+pickle.dump(pl, open(pick_file, "wb"))  # saving the plot data in order nto to connect to the db everytime
+
+
+# print(isos[0], runs[1], pl[isos[0]][runs[0]])
+# print(isos[1], runs[1], pl[isos[1]][runs[0]])
 
 """ plotting of combined tracks. """
-# pl = pickle.load(open(pick_file, "rb"))
+pl = pickle.load(open(pick_file, "rb"))
+for par in pars: # , 'sigma', 'Int0']:
+    for iso in isos:
+        plot.clear()
+        print('plotting ', iso)
+        s0t0_l = list(pl[iso][runs[0]][par])[:-1]
+        s0t0_l.append(('k.', 'k'))
+        s0t1_l = list(pl[iso][runs[1]][par])[:-1]
+        s0t1_l.append(('bo', 'b'))
+        s0t2_l = list(pl[iso][runs[2]][par])[:-1]
+        s0t2_l.append(('r+', 'r'))
+        plot.plotAverage(*s0t0_l)
+        plot.plotAverage(*s0t1_l)
+        plot.plotAverage(*s0t2_l)
+        ax = plot.get_current_axes()
+        fig = plot.get_current_figure()
+        fig.set_size_inches(10, 8, forward=True)
+        # plot.show(True)
 
-# for par in ['center', 'sigma', 'Int0']:
-#     for s in range(0, 2):
-#         plot.clear()
-#         s0t0_l = list(pl['s' + str(s) + 't0'][par])[:-1]
-#         s0t0_l.append(('k.', 'k'))
-#         s0t1_l = list(pl['s' + str(s) + 't1'][par])[:-1]
-#         s0t1_l.append(('bo', 'b'))
-#         s0t2_l = list(pl['s' + str(s) + 't2'][par])[:-1]
-#         s0t2_l.append(('r+', 'r'))
-#         plot.plotAverage(*s0t0_l)
-#         plot.plotAverage(*s0t1_l)
-#         plot.plotAverage(*s0t2_l)
-#         ax = plot.get_current_axes()
-#         fig = plot.get_current_figure()
-#         fig.set_size_inches(10, 8, forward=True)
-#
-#         black_patch = mpatches.Patch(color='black', label='-2V to -1.7V')
-#         blue_patch = mpatches.Patch(color='blue', label='inverting')
-#         red_patch = mpatches.Patch(color='red', label='-1.7V to -2V')
-#         plot.get_current_axes().legend(handles=[black_patch, blue_patch, red_patch],
-#                                        bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-#                                        ncol=3, mode="expand", borderaxespad=0.)
-#         plot.get_current_axes().text(0.95, 0.01, 's' + str(s) + '_t0_to_t3_' + par,
-#                                      verticalalignment='bottom', horizontalalignment='right',
-#                                      transform=ax.transAxes,
-#                                      color='green', fontsize=20)
-#         plot.save(os.path.join(combine_plots_dir,
-#                                's' + str(s) + '_t0_to_t3_' + par + '.png'))
-#
-#         # plot.show(True)
 
+        black_patch = mpatches.Patch(color='black', label='sc0')
+        blue_patch = mpatches.Patch(color='blue', label='sc1')
+        red_patch = mpatches.Patch(color='red', label='sc0+sc1')
+        plot.get_current_axes().legend(handles=[black_patch, blue_patch, red_patch],
+                                       bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+                                       ncol=3, mode="expand", borderaxespad=0.)
+        plot.get_current_axes().text(0.95, 0.01, iso + '_' + par,
+                                     verticalalignment='bottom', horizontalalignment='right',
+                                     transform=ax.transAxes,
+                                     color='green', fontsize=20)
+        plot.save(os.path.join(combine_plots_dir,
+                               iso + '_' + par + '.png'))
+# #
+#         plot.show(True)
+#
 
 ''' NOW IT IS REFERENCED ON ONE TRACK AND THE OTHERS ARE PLOTTED AS A DIFFERENCE TO IT. '''
 # ref = 's1t0'
