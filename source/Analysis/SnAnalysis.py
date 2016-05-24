00e9858dea34037bd9f4a83c2a274f0c83b53a36
chgorges <chgorges@uni-mainz.de>
2016-05-24 12:36:40 +0200

    possibilty to fit with isovar!

--- source/Analysis/SnAnalysis.py
+++ source/Analysis/SnAnalysis.py
@@ -23,34 +23,34 @@ db = 'V:/Projekte/COLLAPS/Sn/Measurement_and_Analysis_Christian/Sn.sqlite'
 
 
 '''Plotting spectra'''
-isoL = []
-for i in range(110,137):
-    isoL.append(str(str(i)+'_Sn'))
-    if i == 117 or i == 121 or i == 125 or i == 127 or i == 129 or i == 130 or i == 131:
-        isoL.append(str(str(i)+'_Sn_m'))
-isoL.append('137_Sn_11_2')
-isoL.append('137_Sn_7_2')
-isoL.append('137_Sn_3_2')
-isoL.append('138_Sn')
-Tools.centerPlot(db,isoL)
-print(Physics.wavelenFromFreq(Physics.freqFromWavenumber(22110.525/2)))
-for i in isoL:
-   Tools.isoPlot(db, i, as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
-                 saving=True, show=False, col=True)
-Tools.isoPlot(db, '117_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
-              saving=True, show=False, col=True, isom_name='117_Sn_m')
-Tools.isoPlot(db, '121_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
-              saving=True, show=False, col=True, isom_name='121_Sn_m')
-Tools.isoPlot(db, '125_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
-              saving=True, show=False, col=True, isom_name='125_Sn_m')
-Tools.isoPlot(db, '127_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
-              saving=True, show=False, col=True, isom_name='127_Sn_m')
-Tools.isoPlot(db, '129_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
-              saving=True, show=False, col=True, isom_name='129_Sn_m')
-Tools.isoPlot(db, '130_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
-              saving=True, show=False, col=True, isom_name='130_Sn_m')
-Tools.isoPlot(db, '131_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
-              saving=True, show=False, col=True, isom_name='131_Sn_m')
+# isoL = []
+# for i in range(110,137):
+#     isoL.append(str(str(i)+'_Sn'))
+#     if i == 117 or i == 121 or i == 125 or i == 127 or i == 129 or i == 130 or i == 131:
+#         isoL.append(str(str(i)+'_Sn_m'))
+# isoL.append('137_Sn_11_2')
+# isoL.append('137_Sn_7_2')
+# isoL.append('137_Sn_3_2')
+# isoL.append('138_Sn')
+# Tools.centerPlot(db,isoL)
+# print(Physics.wavelenFromFreq(Physics.freqFromWavenumber(22110.525/2)))
+# for i in isoL:
+#    Tools.isoPlot(db, i, as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
+#                  saving=True, show=False, col=True)
+# Tools.isoPlot(db, '117_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
+#               saving=True, show=False, col=True, isom_name='117_Sn_m')
+# Tools.isoPlot(db, '121_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
+#               saving=True, show=False, col=True, isom_name='121_Sn_m')
+# Tools.isoPlot(db, '125_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
+#               saving=True, show=False, col=True, isom_name='125_Sn_m')
+# Tools.isoPlot(db, '127_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
+#               saving=True, show=False, col=True, isom_name='127_Sn_m')
+# Tools.isoPlot(db, '129_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
+#               saving=True, show=False, col=True, isom_name='129_Sn_m')
+# Tools.isoPlot(db, '130_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
+#               saving=True, show=False, col=True, isom_name='130_Sn_m')
+# Tools.isoPlot(db, '131_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(22110.525),
+#               saving=True, show=False, col=True, isom_name='131_Sn_m')
 
 '''Crawling'''
 # Tools.crawl(db)
@@ -61,8 +61,8 @@ Tools.isoPlot(db, '131_Sn', as_freq=False, laserfreq=Physics.freqFromWavenumber(
 # Analyzer.combineRes('Kepco', 'b', 'Run2', db, False)
 
 '''Fitting the spectra with Voigt-Fits!'''
-#run = 'Run0'
-#BatchFit.batchFit(Tools.fileList(db,'120_Sn'), db,run)
+run = 'Run0'
+BatchFit.batchFit(Tools.fileList(db,'117_Sn'), db,run)
 
 '''Mean of center, sigma and gamma for 120_Sn'''
 # Analyzer.combineRes('120_Sn', 'gamma',run, db)
