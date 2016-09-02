80b88e30866b6f53f7fea05d4ac2e746fa5c8213
skaufmann <simkaufm@uni-mainz.de>
2016-09-02 10:53:28 +0200

    fixed kepco scan issue. also moved kepco scna back to cs if no sequencer is selected. Still the triggering for 2 or more dmms must be included properly

--- PolliFit/source/MPLPlotter.py
+++ PolliFit/source/MPLPlotter.py
@@ -32,6 +32,16 @@ def plot(*args):
 
 
 def plotFit(fit, color='-r', x_in_freq=True, plot_residuals=True):
+    kepco = False
+    if fit.meas.type == 'Kepco':
+        x_in_freq = False
+        kepco = True
+    try:
+        if fit.meas.seq_type == 'kepco':
+            x_in_freq = False
+            kepco = True
+    except Exception:
+        pass
     if x_in_freq:
         data = fit.meas.getArithSpec(*fit.st)
         for i, e in enumerate(data[0]):
@@ -62,6 +72,8 @@ def plotFit(fit, color='-r', x_in_freq=True, plot_residuals=True):
     ax1.set_ylabel('cts / a.u.')
     if x_in_freq:
         plt.xlabel('relative frequency / MHz')
+    elif kepco:
+        plt.xlabel('Line Voltage / V')
     else:
         plt.xlabel('Ion kinetic energy / eV')
 
