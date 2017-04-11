a41b1aee2ed6d160b7d2763c4e3bf8b692526421
sikaufma <simkaufm@uni-mainz.de>
2017-04-12 00:31:54 +0200

    Release V1.17: - a lot of bugfixes compared to 1.16 - improved speed of Agilents 34461A - new isotopes can take settings from other isotopes in db

--- TildaHost/source/Application/Config.py
+++ TildaHost/source/Application/Config.py
@@ -9,8 +9,8 @@ Created on '23.09.2015'
 File for global Configs of Tilda
 """
 
-version = '1.16'
-versiondate = '09.04.2017'
+version = '1.17'
+versiondate = '12.04.2017'
 _main_instance = None
 # _main_instance is a global variable to store the main instance and make it accessible from everywhere.
 # https://docs.python.org/3.4/faq/programming.html#how-do-i-share-global-variables-across-modules
