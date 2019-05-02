16b58e751b3d2e58c042bd93fbf278631df7c6ba
pimgram <pimgram@ikp.tu-darmstadt.de>
2019-05-02 14:56:21 +0200

    added option charge=1 for diffDoppler().

--- PolliFit/source/Physics.py
+++ PolliFit/source/Physics.py
@@ -71,14 +71,14 @@ def wavelenFromFreq(frequency):
     ''' Returns the wavelength in nm at a given frequency/MHz '''
     return c / (frequency * 10 ** 6) * 10 ** 9
 
-def diffDoppler(nu_0, volt, m, real=False):
+def diffDoppler(nu_0, volt, m, charge=1, real=False):
     '''returns the differential doppler Factor [MHZ/V]'''
     if real:
-        f_L = nu_0 * (1 + volt * qe / (m * u * c ** 2) * (1 - (1 + 2 * m * u * c ** 2 / (volt * qe)) ** 0.5))
-        du = m * u * c ** 2 * (f_L ** 2 - nu_0 ** 2) / (2 * nu_0 * qe * f_L ** 2) # Real calc. see Diss. K. König
+        f_L = nu_0 * (1 + volt * qe * charge / (m * u * c ** 2) * (1 - (1 + 2 * m * u * c ** 2 / (volt * qe * charge)) ** 0.5))
+        du = m * u * c ** 2 * (f_L ** 2 - nu_0 ** 2) / (2 * nu_0 * qe * charge * f_L ** 2) # Real calc. see Diss. K. König
         dv = -1/du
     else:
-        dv = nu_0 * qe / np.sqrt(2 * qe * volt * m * u * c ** 2) # old approx.
+        dv = nu_0 * qe / np.sqrt(2 * qe * charge * volt * m * u * c ** 2) # old approx.
 
     return dv
 
