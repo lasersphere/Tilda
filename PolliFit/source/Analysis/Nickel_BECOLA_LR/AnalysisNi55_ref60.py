bb64c11c84a339bc2da40ef93bbbf582398247b8
Laura Renth <lrenth@ikp.tu-darmstadt.de>
2020-07-14 11:50:05 +0200

    Analysis 55Ni ref 60Ni Prep LR

--- PolliFit/source/Analysis/Nickel_BECOLA_LR/AnalysisNi55_ref60.py
+++ PolliFit/source/Analysis/Nickel_BECOLA_LR/AnalysisNi55_ref60.py
@@ -1,11 +1,11 @@
-import sqlite3
-import numpy as np
-import Physics
 import os
-import ast
-from Measurement.XMLImporter import XMLImporter
-import BatchFit
+import sqlite3
 from matplotlib import pyplot as plt
+import ast
+import numpy as np
+from Measurement.XMLImporter import XMLImporter
+import Physics
+import BatchFit
 
 
 class NiAnalysis:
@@ -58,7 +58,7 @@ class NiAnalysis:
             file_uncert = np.sqrt(file_uncert)
             center.append(file_center)
             uncert.append(file_uncert)
-        plt.errorbar([6192, 6208, 6243, 6254], center, yerr=uncert)
+        plt.errorbar([6363, 6396, 6419, 6463, 6466, 6502], center, yerr=uncert)
         plt.title('Center uncalibrated')
         plt.show()
 
@@ -91,64 +91,14 @@ class NiAnalysis:
         for u in uncert:
             weights.append(1 / (u ** 2))
         mean = np.average(center, weights=weights)
-        plt.errorbar([6192, 6208, 6243, 6254], center, yerr=uncert)
-        plt.plot([6192,6254],[mean, mean])
-        plt.plot([6192, 6254], [self.frequ_60ni, self.frequ_60ni])
+        plt.errorbar([6363, 6396, 6419, 6463, 6466, 6502], center, yerr=uncert)
+        plt.plot([6363, 6502],[mean, mean])
+        plt.plot([6363, 6502], [self.frequ_60ni, self.frequ_60ni])
         plt.title('Center calibrated')
         plt.show()
 
-        # assign calibration
-        self.assign_cal()
-        print('-------------------- Assignement done')
-
-        # fit all files
-        for run in self.runs:
-            for tup in self.ref_groups:
-                file = 'BECOLA_' + str(tup[1]) + '.xml'
-                print('Start fit')
-                self.fit_all([file],run)
-            for tup in self.cal_groups:
-                for f in tup[1:]:
-                    file = 'BECOLA_' + str(f) + '.xml'
-                    print('Fitting file', f)
-                    self.fit_all([file],run)
-
-        # plot isotope shift of reference
-        for run in self.runs:
-            ishifts = []
-            uncert = []
-            plot_files = []
-            for tup in self.ref_groups:
-                ishifts.append(self.calc_iso_shift(tup, run)[0])
-                uncert.append(self.calc_iso_shift(tup, run)[1])
-                plot_files.append(tup[0])
-            plt.errorbar(plot_files, ishifts, yerr=uncert)
-            plt.title('calibratet isotope shifts scaler' + run)
-            plt.show()
-
     def fit_all(self, files, run):
         for f in files:
-            print('Fitting File', f)
-            for tup in self.cal_groups:
-                for element in tup[1:]:
-                    if 'BECOLA_' + str(element) + '.xml' == f:
-                        file60 = 'BECOLA_' + str(tup[0]) + '.xml'
-                        con = sqlite3.connect(self.db)
-                        cur = con.cursor()
-                        cur.execute('''SELECT pars from FitRes WHERE file = ? AND run = ?''', (file60, run))
-                        fitpars = ast.literal_eval(cur.fetchall()[0][0])
-                        asy = fitpars['asy'][0]
-                        cur.execute('''SELECT shape FROM Lines WHERE refRun = ?''', (run,))
-                        setpars = ast.literal_eval(cur.fetchall()[0][0])
-                        setpars['asy'] = asy
-                        print('The Asymmetry Factor of file', file60 , ' and run', run, 'is', asy)
-                        cur.execute('''UPDATE Lines SET shape = ?''', (str(setpars),))
-                        cur.execute('''SELECT fixShape FROM Lines WHERE refRun = ?''', (run,))
-                        fixShape = ast.literal_eval(cur.fetchall()[0][0])
-                        fixShape['asy'] = True
-                        cur.execute('''UPDATE Lines SET fixShape = ?''',(str(fixShape),))
-                        con.commit()
-                        con.close()
             for tup in self.ref_groups:
                 if 'BECOLA_' + str(tup[1]) + '.xml' == f:
                     file60 = 'BECOLA_' + str(tup[0]) + '.xml'
@@ -178,6 +128,7 @@ class NiAnalysis:
             index = list(spec.cts[0][0]).index(max(spec.cts[0][0]))
             con = sqlite3.connect(self.db)
             cur = con.cursor()
+            print(f)
             cur.execute('''SELECT accVolt FROM Files WHERE file = ?''', (f,))
             accV = cur.fetchall()[0][0]
             center_voltage = accV - spec.x[0][index]
@@ -194,7 +145,7 @@ class NiAnalysis:
                 mass = 60
             else:
                 mass = 56
-            cur.execute('''SELECT frequency FROM Lines WHERE refRun = ? ''',(run,))
+            cur.execute('''SELECT frequency FROM Lines WHERE refRun = ? ''', (run,))
             frequ = cur.fetchall()[0][0]
             v = Physics.relVelocity(Physics.qe * center_voltage, mass * Physics.u)
             v = -v
@@ -205,14 +156,14 @@ class NiAnalysis:
             self.adj_center(center, iso)
 
             # Fit
-            print('Run to fit with:',run)
+            print('Run to fit with:', run)
             BatchFit.batchFit(np.array([f]), self.db, run)
 
             # Reset FixShape
             cur.execute('''SELECT fixShape FROM Lines WHERE refRun = ?''', (run,))
             fixShape = ast.literal_eval(cur.fetchall()[0][0])
-            fixShape['asy'] = [0,20]
-            cur.execute('''UPDATE Lines SET fixShape = ? WHERE refRun = ?''', ( str(fixShape), run,))
+            fixShape['asy'] = [0, 20]
+            cur.execute('''UPDATE Lines SET fixShape = ? WHERE refRun = ?''', (str(fixShape), run,))
             con.commit()
             con.close()
 
@@ -272,6 +223,14 @@ class NiAnalysis:
             con.commit()
             con.close()
 
+    def get_files(self, isotope):
+        con = sqlite3.connect(self.db)
+        cur = con.cursor()
+        cur.execute('''SELECT file FROM Files WHERE type LIKE ? ''', (isotope,))
+        files = cur.fetchall()
+        con.close()
+        return [f[0] for f in files]
+
     def calibrate(self, file, frequ):
         delta_frequ = frequ - self.frequ_60ni
 
@@ -287,109 +246,19 @@ class NiAnalysis:
         delta_u = delta_frequ / diffDopp60
         return accVolt + delta_u
 
-    def get_files(self, isotope):
-        con = sqlite3.connect(self.db)
-        cur = con.cursor()
-        cur.execute('''SELECT file FROM Files WHERE type LIKE ? ''', (isotope,))
-        files = cur.fetchall()
-        con.close()
-        return [f[0] for f in files]
-
-    def assign_cal(self):
-        con = sqlite3.connect(self.db)
-        cur = con.cursor()
-        for tup in self.ref_groups:
-            file60 = 'BECOLA_' + str(tup[0]) + '.xml'
-            file58 = 'BECOLA_' + str(tup[1]) + '.xml'
-            print(file60)
-            cur.execute('''SELECT accVolt FROM Files WHERE file = ?''', (file60,))
-            accVolt = cur.fetchall()[0][0]
-            cur.execute('''UPDATE Files SET accVolt = ? WHERE file = ?''', (accVolt, file58,))
-        for tup in self.cal_groups:
-            file60 = 'BECOLA_' + str(tup[0]) + '.xml'
-            cur.execute('''SELECT accVolt FROM Files WHERE file = ?''', (file60,))
-            accVolt = cur.fetchall()[0][0]
-            for file in tup[1:]:
-                file56 = 'BECOLA_' + str(file) + '.xml'
-                cur.execute('''UPDATE Files SET accVolt = ? WHERE file = ?''', (accVolt, file56,))
-        con.commit()
-        con.close()
-
-    def calc_iso_shift(self, files, run):
-        # files = (60, A)
-        # get center of reference
-        file60 = 'BECOLA_' + str(files[0]) + '.xml'
-        con = sqlite3.connect(self.db)
-        cur = con.cursor()
-        cur.execute('''SELECT pars FROM FitRes WHERE file = ? and run = ?''', (file60, run,))
-        center60 = ast.literal_eval(cur.fetchall()[0][0])['center']
-        con.close()
-
-        # get center of interest
-        file_int = 'BECOLA_' + str(files[1]) + '.xml'
-        con = sqlite3.connect(self.db)
-        cur = con.cursor()
-        cur.execute('''SELECT pars FROM FitRes WHERE file = ? and run = ?''', (file_int, run,))
-        center_int = ast.literal_eval(cur.fetchall()[0][0])['center']
-        con.close()
-
-        # Calculate isotope shift
-        ishift = center_int[0] - center60[0]
-        print('Isotope shift is', ishift)
-
-        # Calculate uncertainty
-        uncert = np.sqrt(np.square(center_int[1]) + np.square(center60[1]))
-
-        return ishift, uncert
-
-    def calc_56Ni(self):
-        files = []
-        ishift = []
-        uncert = []
-        for i, run in enumerate(self.runs):
-            for tup in self.cal_groups:
-                ref_file = 'BECOLA_' + str(tup[0]) + '.xml'
-                con = sqlite3.connect(self.db)
-                cur = con.cursor()
-                cur.execute('''SELECT pars FROM FitRes WHERE run = ? AND file = ?''', (run, ref_file,))
-                center_pars = ast.literal_eval(cur.fetchall()[0][0])['center']
-                con.close()
-                center_ref = center_pars[0]
-                uncert_ref = center_pars[1]
-                for f in tup[1:]:
-                    file = 'BECOLA_' + str(f) + '.xml'
-                    con = sqlite3.connect(self.db)
-                    cur = con.cursor()
-                    cur.execute('''SELECT pars FROM FitRes WHERE run = ? AND file = ?''', (run, file,))
-                    center_pars = ast.literal_eval(cur.fetchall()[0][0])['center']
-                    con.close()
-                    ishift.append(center_pars[0] - center_ref)
-                    uncert.append(np.sqrt(uncert_ref ** 2 + center_pars[1] ** 2))
-                    files.append(float(str(i) + str(f)))
-        xaxis = list(range(0, len(files)))
-        print(files)
-        weights = []
-        for u in uncert:
-            weights.append(1 / u ** 2)
-        mean = np.average(ishift, weights=weights)
-        print('Weighted average of isotope shifts is', mean)
-        sigma = np.std(ishift)
-        print('Standard deviation is', sigma)
-        plt.errorbar(xaxis, ishift, yerr=uncert, fmt='b.')
-        plt.plot([xaxis[0], xaxis[-1]], [mean, mean])
-        plt.fill_between([xaxis[0], xaxis[-1]], [mean + sigma, mean + sigma], y2=[mean - sigma, mean - sigma], color='r', alpha=0.2)
-        plt.title('Isotope shift of 56Ni')
-        plt.ylabel('MHz')
-        plt.show()
 
 working_dir = 'D:\\Daten\\IKP\\Nickel-Auswertung\\Auswertung'
-db = 'Nickel_BECOLA_60Ni-56Ni.sqlite'
+db = 'Nickel_BECOLA_60Ni-55Ni.sqlite'
 line_vars = ['58_0','58_1','58_2']
 runs = ['AsymVoigt0', 'AsymVoigt1', 'AsymVoigt2']
 frequ_60ni = 850344183
-reference_groups = [(6192, 6191), (6208, 6207), (6243, 6242), (6254, 6253)]
-calibration_groups = [(6192, 6202, 6203, 6204), (6208, 6211, 6213, 6214), (6243, 6238, 6239, 6240), (6254, 6251, 6252)]
+reference_groups = [(6363, 6362), (6396, 6395), (6419, 6417), (6463, 6462), (6466, 6467), (6502, 6501)]
+calibration_groups = [((6363, 6396), (6369, 6373, 6370, 6375, 6376, 6377, 6378, 6380, 6382, 6383, 6384, 6387, 6391,
+                                      6392, 6393)),
+                      ((6396, 6419), (6399, 6400, 6401, 6402, 6404, 6405, 6406, 6408, 6410, 6411, 6412)),
+                      ((6419, 6463), (6428, 6429, 6430, 6431, 6432, 6433, 6434, 6436, 6438, 6440, 6441, 6444, 6445,
+                                      6447, 6448)),
+                      ((6466, 6502), (6468, 6470, 6471, 6472, 6473, 6478, 6479, 6480, 6493))]
 niAna = NiAnalysis(working_dir, db, line_vars, runs, frequ_60ni, reference_groups, calibration_groups)
 niAna.reset()
 niAna.prep()
-niAna.calc_56Ni()
