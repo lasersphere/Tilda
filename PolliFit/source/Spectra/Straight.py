c6e81cbb5095e16fd8c79e43f097dc9c6a0b3f5f
Patrick Müller <pamueller@ikp.tu-darmstadt.de>
2021-09-18 19:33:35 +0200

    - Fixed the 'Straight' spectrum to be compatible with numpy arrays.

--- PolliFit/source/Spectra/Straight.py
+++ PolliFit/source/Spectra/Straight.py
@@ -1,19 +1,20 @@
-'''
+"""
 Created on 23.03.2014
+edited on 18.09.2021
 
-@author: hammen
-'''
+@author: hammen, pamueller
+"""
 
 import numpy as np
 
+
 class Straight(object):
-    '''
-    A straight as lineshape object
-    '''
+    """
+    A straight as lineshape object.
+    """
 
     def __init__(self):
         self.nPar = 2
-        self.nFreePar = 2
         self.pb = 0
         self.pm = 1
         self.x_min = None
@@ -21,20 +22,18 @@ class Straight(object):
 
     def evaluate(self, x, p):
         if self.x_max is None:
-            self.x_max = x
-            self.x_min = x
-        self.x_min = min(x, self.x_min)
-        self.x_max = max(x, self.x_max)
-        return p[self.pb] + x*p[self.pm]
+            self.x_min = np.min(x)
+            self.x_max = np.max(x)
+        return p[self.pb] + x * p[self.pm]
     
     def evaluateE(self, e, freq, col, p):
         return self.evaluate(e, p)
 
     def leftEdge(self):
-        return self.x_min - abs((self.x_min - self.x_max) * 0.001)
+        return self.x_min - np.abs(self.x_min - self.x_max) * 0.001
 
     def rightEdge(self):
-        return self.x_max + abs((self.x_min - self.x_max) * 0.001)
+        return self.x_max + np.abs(self.x_min - self.x_max) * 0.001
 
     def getPars(self):
         return [0, 1]
@@ -51,10 +50,13 @@ class Straight(object):
     def parAssign(self):
         return [('Kepco', [True, True])]
     
-    def getBlankNames(self):
-        return ['b', 'm']
-    
-    def toPlotE(self, freq, col, p, prec = 10000):
-        '''Return ([x/V], [y/V]) values with prec number of points'''
-        self.recalc(p)
-        return ([x for x in np.linspace(self.leftEdge(), self.rightEdge(), prec)], [self.evaluate(x, p) for x in np.linspace(self.leftEdge(), self.rightEdge(), prec)])
+    def toPlotE(self, freq, col, p, prec=10000):
+        """
+        :param freq: The
+        :param col: Whether its a collinear spectrum.
+        :param p: The parameter list.
+        :param prec: The number of data points.
+        :returns: (x in V, y in V) values with 'prec' number of points.
+        """
+        x = np.linspace(self.leftEdge(), self.rightEdge(), prec)
+        return x, self.evaluate(x, p)
