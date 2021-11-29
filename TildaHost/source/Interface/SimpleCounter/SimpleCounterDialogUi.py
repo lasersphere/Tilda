0da1faa61003337ddaefa511f5be11188a1e6c67
Laura Renth <laurenath@gmail.com>
2021-11-29 16:27:20 +0100

    Worked on UI: * included DropDown for triggerSelection * depending on selected Trigger, trigger-Parameter menue is shown

--- TildaHost/source/Interface/SimpleCounter/SimpleCounterDialogUi.py
+++ TildaHost/source/Interface/SimpleCounter/SimpleCounterDialogUi.py
@@ -12,6 +12,9 @@ from PyQt5 import QtWidgets, QtGui
 from Interface.SimpleCounter.Ui_Simp_Count_Dial import Ui_Dialog_simpleCounterControl
 import Application.Config as Cfg
 import ast
+from Driver.DataAcquisitionFpga.TriggerTypes import TriggerTypes as TiTs
+import Interface.TriggerWidgets.FindDesiredTriggerWidg as FindDesiredTriggerWidg
+#import Interface.SimpleCounter.no_TG_widget as no_tg_widget
 
 
 class SimpleCounterDialogUi(QtWidgets.QDialog, Ui_Dialog_simpleCounterControl):
@@ -21,6 +24,8 @@ class SimpleCounterDialogUi(QtWidgets.QDialog, Ui_Dialog_simpleCounterControl):
         self.start = False
         self.act_pmts = []
         self.datapoints = 0
+        self.buffer_pars = {'type': TiTs.no_trigger}
+        self.trigger_widget = None
 
         self.setupUi(self)
 
@@ -28,10 +33,17 @@ class SimpleCounterDialogUi(QtWidgets.QDialog, Ui_Dialog_simpleCounterControl):
         self.buttonBox.rejected.connect(self.close)
         self.lineEdit_act_pmts.textChanged.connect(self.set_act_pmts)
         self.spinBox_plotpoints.valueChanged.connect(self.set_datapoints)
+        self.comboBox_triggerSelect.currentTextChanged.connect(self.trigger_select)
 
         ''' default vals: '''
         self.lineEdit_act_pmts.setText('0, 1, 2, 3')
         self.spinBox_plotpoints.setValue(600)
+        self.comboBox_triggerSelect.addItems(tr.name for tr in TiTs)
+        trig_type = TiTs.no_trigger
+        self.comboBox_triggerSelect.setCurrentText(trig_type.name)
+        self.trigger_widget = FindDesiredTriggerWidg.find_trigger_widget(self.buffer_pars)
+        self.verticalLayout_trigger.replaceWidget(self.widget_trigger_place_holder, self.trigger_widget)
+        #self.verticalLayout.replaceWidget(self.widget_timeGate_place_holder,)
 
         self.exec_()
 
@@ -55,5 +67,17 @@ class SimpleCounterDialogUi(QtWidgets.QDialog, Ui_Dialog_simpleCounterControl):
     def set_datapoints(self, val):
         self.datapoints = val
         self.label_plotpoints_set.setText(str(val))
+
+    def trigger_select(self, trig_str):
+        """
+        finds the desired trigger widget and sets it into self.trigger_widget
+        """
+        self.buffer_pars['type'] = getattr(TiTs, trig_str)
+        self.verticalLayout_trigger.removeWidget(self.trigger_widget)
+
+        if self.trigger_widget is not None:
+            self.trigger_widget.setParent(None)
+        self.trigger_widget = FindDesiredTriggerWidg.find_trigger_widget(self.buffer_pars)
+        self.verticalLayout_trigger.addWidget(self.trigger_widget)
 
 
--- TildaHost/source/Interface/TrackParUi/Ui_TrackPar_Wide.py
+++ TildaHost/source/Interface/TrackParUi/Ui_TrackPar_Wide.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\Ui_TrackPar_Wide.ui'
+# Form implementation generated from reading ui file 'Ui_TrackPar_Wide.ui'
 #
-# Created by: PyQt5 UI code generator 5.5
+# Created by: PyQt5 UI code generator 5.5.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -679,4 +679,14 @@ class Ui_MainWindowTrackPars(object):
         self.pushButton_cancel.setText(_translate("MainWindowTrackPars", "cancel"))
         self.pushButtonResetToDefault.setText(_translate("MainWindowTrackPars", "reset to default"))
         self.pushButton_confirm.setText(_translate("MainWindowTrackPars", "confirm"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindowTrackPars = QtWidgets.QMainWindow()
+    ui = Ui_MainWindowTrackPars()
+    ui.setupUi(MainWindowTrackPars)
+    MainWindowTrackPars.show()
+    sys.exit(app.exec_())
 
