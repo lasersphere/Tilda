SpectraFit:   - Optimized the fit result prints.   - Linked fitting disables the MC-covariance estimation now (if activated).   - Implemented using bounds and linked fitting together.   - Fixed a bug that prevented using expressions and linked fitting together.   - PolliFit no longer crashes when the database is not open for writing.   - Added the used 'run' to the filenames of saved ascii and plot files.   - Removed the date and clock time from the filename of saved ascii files.

PolliFit/source/Fitter.py
@@ -4,6 +4,7 @@ Created on 20.02.2022
 @author: Patrick Mueller
 """
 
+import os
 import numpy as np
 import itertools as it
 from PyQt5.QtCore import QObject, pyqtSignal
@@ -34,13 +35,14 @@ class Fitter(QObject):
 
     finished = pyqtSignal()
 
-    def __init__(self, models, meas, st, iso, config):
+    def __init__(self, models, meas, st, iso, config=None, run=None):
         """
         :param models: A list of models.
         :param meas: A list of SpecData objects.
         :param st: A list of scaler and track info.
         :param iso: A list of isotopes used for the axis conversion of each SpecData object.
         :param config: A dictionary with information for the fit.
+        :param run: The run of the fitter. This is just used as an info. The default value is ''.
         """
         super().__init__()
         self.models = models
@@ -48,6 +50,11 @@ class Fitter(QObject):
         self.st = st
         self.iso = iso
         self.config = config
+        if self.config is None:
+            self.config = dict(routine='curve_fit', absolute_sigma=False, guess_offset=True,
+                               cov_mc=False, samples_mc=100, arithmetics=None,
+                               summed=False, linked=False)
+        self.run = '' if run is None else run
         self.size = len(self.meas)
         self.n_scaler = min(min(meas.nrScalers if isinstance(meas.nrScalers, list) else [meas.nrScalers])
                             for meas in self.meas)  # The minimum number of scalers for all files and tracks.
@@ -95,6 +102,7 @@ class Fitter(QObject):
                 cts = [np.array(meas.cts[st[1]][scaler]) for scaler in st[0]]
             y_samples = {'s{}'.format(i): np.random.normal(loc=cts[i], scale=np.sqrt(cts[i]), size=(n, cts[i].size))
                          for i in range(self.n_scaler) if 's{}'.format(i) in self.config['arithmetics']}
+            # noinspection PyTypeChecker
             self.yerr.append(np.std(eval(self.config['arithmetics'], y_samples), axis=0, ddof=1))
 
     def gen_data(self):
@@ -184,7 +192,7 @@ class Fitter(QObject):
         errs = []
         chi2 = []
         popt, pcov = [], []
-        for i, (model, x, y, yerr) in enumerate(zip(self.models, self.x, self.y, self.yerr)):
+        for i, (meas, model, x, y, yerr) in enumerate(zip(self.meas, self.models, self.x, self.y, self.yerr)):
             try:
                 if model.error:
                     raise ValueError(model.error)
@@ -195,7 +203,6 @@ class Fitter(QObject):
                         model.guess_offset(x, y)
                 fixed, bounds = model.fit_prepare()
                 if self.config['cov_mc']:
-                    print('-----------------MC-----------------')
                     y_samples = np.random.normal(y, yerr, size=(self.config['samples_mc'], y.size))
                     ps = np.array([routine(model, x, y_sample, p0=model.vals, p0_fixed=fixed, sigma=yerr,
                                            absolute_sigma=self.config['absolute_sigma'], bounds=bounds, report=False,
@@ -213,11 +220,16 @@ class Fitter(QObject):
                     pt = np.array(model.update_args(pt))
                 model.set_vals(pt, force=True)
                 chi2.append(self.reduced_chi2(i))  # Calculate chi2 after the vals are set.
-                for name, val, err in zip(model.names, pt, np.sqrt(np.diag(pc))):
-                    print('{}: {} +/- {}'.format(name, val, err))
-                print('Cov. Matrix:')
+
+                print_colored('HEADER', '\nFit of file \'{}\':'.format(os.path.splitext(meas.file)[0]))
+                digits = int(np.floor(np.log10(np.abs(model.size)))) + 1
+                print('Optimized parameters:')
+                for j, (name, val, err) in enumerate(zip(model.names, pt, np.sqrt(np.diag(pc)))):
+                    print('{}:   {} = {} +/- {}'.format(str(j).zfill(digits), name, val, err))
+                print('\nCov. Matrix:')
                 print_cov(pc, normalize=True, decimals=2)
-                print('Red. chi2: {}'.format(chi2[-1]))
+                print('\nRed. chi2 = {}'.format(np.around(chi2[-1], decimals=2)))
+
                 popt.append(pt)
                 pcov.append(pc)
                 if np.any(np.isinf(pc)):
@@ -239,7 +251,7 @@ class Fitter(QObject):
             color = 'WARNING'
         if len(errs) > 0:
             color = 'FAIL'
-        print_colored(color, 'Fits completed, success in {} / {}.'.format(self.size - len(warn), self.size))
+        print_colored(color, '\nFits completed, success in {} / {}.'.format(self.size - len(warn), self.size))
         info = dict(warn=warn, errs=errs, chi2=chi2)
         return popt, pcov, info
 
@@ -275,10 +287,18 @@ class Fitter(QObject):
             pt = np.array(model.update_args(pt))
             model.set_vals(pt, force=True)  # Set the vals of the model (auto sets the vals of the linked models).
             chi2 = self.reduced_chi2()  # Calculate chi2 after the vals are set.
-            for name, val, err in zip(model.names, pt, np.sqrt(np.diag(pc))):
-                print('{}: {} +/- {}'.format(name, val, err))
+
+            print_colored('HEADER', '\nLinked fit of files {}'
+                          .format([os.path.splitext(meas.file)[0] for meas in self.meas]))
+            digits = int(np.floor(np.log10(np.abs(model.size)))) + 1
+            for i, (name, val, err) in enumerate(zip(model.names, pt, np.sqrt(np.diag(pc)))):
+                print('{}:   {} = {} +/- {}'.format(str(i).zfill(digits), name, val, err))
+            print('\nCov. Matrix:')
+            print_cov(pc, normalize=True, decimals=2)
+            print('\nRed. chi2:')
             for i, _chi2 in enumerate(chi2):
-                print('{} Red. chi2: {}'.format(str(i).zfill(int(np.log10(self.size))), _chi2))
+                print('{}:   {}'.format(str(i).zfill(int(np.log10(self.size))), np.around(_chi2, decimals=2)))
+
             popt = [pt[_slice] for _slice in model.slices]
             pcov = [pc[_slice, _slice] for _slice in model.slices]
         except (ValueError, RuntimeError) as e:
@@ -292,11 +312,11 @@ class Fitter(QObject):
             if _model.type == 'Offset':
                 _model.update_on_call = True  # Reset all offset models to be updated on call.
         if len(warn) == len(errs) == 0:
-            print_colored('OKGREEN', 'Linked fit completed, success.')
+            print_colored('OKGREEN', '\nLinked fit completed, success.')
         elif len(errs) > 0:
-            print_colored('FAIL', 'Linked fit completed, failed.')
+            print_colored('FAIL', '\nLinked fit completed, failed.')
         elif len(warn) > 0:
-            print_colored('WARNING', 'Linked fit completed, warning.')
+            print_colored('WARNING', '\nLinked fit completed, warning.')
         info = dict(warn=warn, errs=errs, chi2=chi2)
         return popt, pcov, info
 

PolliFit/source/MPLPlotter.py
@@ -349,9 +349,11 @@ def plot_model_fit(fitter, index, x_as_freq=True, plot_summands=True, plot_npeak
     labels = [each.get_label() for each in lines]
     fig.legend(lines, labels, loc='upper center', ncol=2, bbox_to_anchor=(0.15, 0.8, 0.8, 0.2), mode='expand',
                fontsize=fontsize + 2, numpoints=1)
+    run = '.{}'.format(fitter.run) if fitter.run else ''
+    filename = '{}{}'.format(os.path.splitext(fitter.meas[index].file)[0], run)
     if ascii_path:
         save_plot_as_ascii(_x, y, y_res, yerr, x_fit, y_fit, *y_fit_extra, save_path=ascii_path,
-                           filename=fitter.meas[index].file, xlabel=ax2.get_xlabel(), labels_extra=labels_extra)
+                           filename=filename, xlabel=ax2.get_xlabel(), labels_extra=labels_extra)
     if plot_path:
         if not os.path.exists(plot_path):
             try:
@@ -362,7 +364,7 @@ def plot_model_fit(fitter, index, x_as_freq=True, plot_summands=True, plot_npeak
                 return
         if fig_save_format[0] == '.':
             fig_save_format = fig_save_format[1:]
-        f = os.path.join(plot_path, '{}.{}'.format(os.path.splitext(fitter.meas[index].file)[0], fig_save_format))
+        f = os.path.join(plot_path, '{}.{}'.format(filename, fig_save_format))
         fig.savefig(f)
     return fig
 
@@ -380,18 +382,20 @@ def save_plot_as_ascii(x, y, y_res, yerr, x_fit, y_fit, *y_fit_extra, save_path=
         labels_extra = ''
     else:
         labels_extra = ', ' + ', '.join(labels_extra)
-    f = os.path.join(save_path, '{}_data_{}'.format(
-        os.path.splitext(filename)[0], datetime.datetime.today().strftime('_%Y-%m-%d_%H-%M-%S.txt')))
+    # f = os.path.join(save_path, '{}.data.{}'.format(
+    #     filename, datetime.datetime.today().strftime('_%Y-%m-%d_%H-%M-%S.txt')))
+    f = os.path.join(save_path, '{}.data.txt'.format(filename))
     np.savetxt(f, np.array([x, y, y_res, yerr]).T, delimiter=', ',
                header='{}, data intensity (counts), fit residuals (counts), data uncertainty (counts)'.format(xlabel))
-    f = os.path.join(save_path, '{}_fit_{}'.format(
-        os.path.splitext(filename)[0], datetime.datetime.today().strftime('_%Y-%m-%d_%H-%M-%S.txt')))
+    # f = os.path.join(save_path, '{}.fit.{}'.format(
+    #     filename, datetime.datetime.today().strftime('_%Y-%m-%d_%H-%M-%S.txt')))
+    f = os.path.join(save_path, '{}.fit.txt'.format(filename))
     np.savetxt(f, np.array([x_fit, y_fit, *y_fit_extra]).T,
                delimiter=', ', header='{}, fit intensity (counts){}'.format(xlabel, labels_extra))
     print('Saved plot as ASCII files in {}.'.format(save_path))
 
 
-def plotMoments(cts, q=True,fontsize_ticks=10):
+def plotMoments(cts, q=True, fontsize_ticks=10):
     if q:
         fig = plt.figure(1, (8, 6))
         fig.clear()

PolliFit/source/SpectraFit.py
@@ -13,8 +13,15 @@ import TildaTools as TiTs
 import MPLPlotter as Plot
 from DBIsotope import DBIsotope
 import Measurement.MeasLoad as MeasLoad
-from Fitter import Fitter
+from Fitter import Fitter, print_colored
 import Models.Collection as Mod
+
+
+def execute(cur, command, *args):
+    try:
+        cur.execute(command, *args)
+    except sqlite3.OperationalError as e:
+        print_colored('FAIL', repr(e))
 
 
 class SpectraFit:
@@ -25,7 +32,7 @@ class SpectraFit:
                  fig_save_format='.png', fmt='.k', fontsize=10):
         self.db = db
         self.files = files
-        self.runs = runs
+        self.runs = runs  # TODO: Use only a single run per instance.
         self.configs = configs
         self.index_config = index_config
 
@@ -58,7 +65,7 @@ class SpectraFit:
     def _execute(self, command, *args):
         con = sqlite3.connect(self.db)
         cur = con.cursor()
-        cur.execute(command, *args)
+        execute(cur, command, *args)
         con.commit()
         con.close()
         
@@ -146,7 +153,7 @@ class SpectraFit:
             else:
                 raise ValueError('File type not supported. The supported types are {}.'.format(self.file_types))
 
-        self.fitter = Fitter(models, meas, st, iso, self.gen_config())
+        self.fitter = Fitter(models, meas, st, iso, self.gen_config(), run=self.runs[0])
         self.load_pars()
         self.reset_model = [[p for p in model.get_pars()] for model in models]
 
@@ -266,8 +273,10 @@ class SpectraFit:
             self.fitter.models[i].update()
 
     def save_pars(self):
-        self._execute('CREATE TABLE IF NOT EXISTS "FitPars"("file" TEXT, "run" TEXT, "softw_gates" TEXT, "config" TEXT,'
-                      ' "pars" TEXT, PRIMARY KEY("file", "run"))')
+        con = sqlite3.connect(self.db)
+        cur = con.cursor()
+        execute(cur, 'CREATE TABLE IF NOT EXISTS "FitPars"("file" TEXT, "run" TEXT, "softw_gates" TEXT, "config" TEXT,'
+                     ' "pars" TEXT, PRIMARY KEY("file", "run"))')
         for i, (file, run, config) in enumerate(zip(self.files, self.runs, self.configs)):
             new_pars = {name: (val, fix, link) for (name, val, fix, link) in self.get_pars(i)}
             pars = TiTs.select_from_db(self.db, 'pars', 'FitPars', [['file', 'run'], [file, run]], caller_name=__name__)
@@ -277,8 +286,10 @@ class SpectraFit:
             if 'inf' in softw_gates:
                 self.set_softw_gates(i, self.fitter.meas[i].softw_gates, -1)
                 softw_gates = str(self.fitter.meas[i].softw_gates)
-            self._execute('INSERT OR REPLACE INTO FitPars (file, run, softw_gates, config, pars)'
-                          ' VALUES (?, ?, ?, ?, ?)', (file, run, softw_gates, str(config), str(new_pars)))
+            execute(cur, 'INSERT OR REPLACE INTO FitPars (file, run, softw_gates, config, pars)'
+                         ' VALUES (?, ?, ?, ?, ?)', (file, run, softw_gates, str(config), str(new_pars)))
+        con.commit()
+        con.close()
 
     def save_fits_to_db(self, popt, pcov, info):
         con = sqlite3.connect(self.db)
@@ -288,8 +299,8 @@ class SpectraFit:
                 continue
             pars = {self.fitter.models[i].names[j]: (pt, np.sqrt(pc[j]), self.fitter.models[i].fixes[j])
                     for j, (pt, pc) in enumerate(zip(popt[i], pcov[i]))}
-            cur.execute('INSERT OR REPLACE INTO FitRes (file, iso, run, rChi, pars) '
-                        'VALUES (?, ?, ?, ?, ?)', (file, self.fitter.iso[i].name, run, info['chi2'][i], str(pars)))
+            execute(cur, 'INSERT OR REPLACE INTO FitRes (file, iso, run, rChi, pars) '
+                         'VALUES (?, ?, ?, ?, ?)', (file, self.fitter.iso[i].name, run, info['chi2'][i], str(pars)))
         con.commit()
         con.close()
 

PolliFit/source/Tools.py
@@ -73,10 +73,11 @@ def print_cov(cov, normalize=False, decimals=2):
         nonzero = norm != 0
         cov[nonzero] /= norm[nonzero]
     cov = np.around(cov + 0., decimals=decimals)
+    digits = int(np.floor(np.log10(np.abs(cov.shape[0])))) + 1
     for i, row in enumerate(cov):
-        print('{}:   {}'.format(i, '   '.join('{}{}{}'.format(get_rgb(*map_corr_coeff_to_color(val)),
-                                                             '{:1.2f}'.format(val).rjust(decimals + 3), COLORS.ENDC)
-                                             for val in row)))
+        print('{}:   {}'.format(str(i).zfill(digits), '   '.join(
+            '{}{}{}'.format(get_rgb(*map_corr_coeff_to_color(val)),
+                            '{:1.2f}'.format(val).rjust(decimals + 3), COLORS.ENDC) for val in row)))
 
 
 def isoPlot(db, iso_name, isovar='', linevar='', as_freq=True, laserfreq=None, col=None, saving=False, show=True,