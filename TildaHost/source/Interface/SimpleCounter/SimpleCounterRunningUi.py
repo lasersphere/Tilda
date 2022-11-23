96a786c38518a7c363cb100e5d64261f11a55bdd
Patrick Müller <pamueller@ikp.tu-darmstadt.de>
2022-11-23 15:56:34 +0100

    SimpleCounterRunningUi, Design overhaul   - Replaced the LCD counter with a spin box to better display count rates > 1 MHz.   - Reordered the control UI elements.   - Increased the heights of the reset and stop buttons.   - Removed the DAC voltage display label since it just displayed the exact same as the double spin box.   - Replaced the labels 'ch{n}' with '{n}' and decreased the required space.

--- TildaHost/source/Interface/SimpleCounter/SimpleCounterRunningUi.py
+++ TildaHost/source/Interface/SimpleCounter/SimpleCounterRunningUi.py
@@ -12,12 +12,37 @@ import os
 import logging
 
 import numpy as np
-from PyQt5 import QtCore
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 
 import Application.Config as Cfg
 import PyQtGraphPlotter as Pg
 from Interface.SimpleCounter.Ui_simpleCounterRunnning import Ui_SimpleCounterRunning
+
+
+LCD_COUNTER = False
+MAGNITUDE = ['', '', ' K', ' M', ' B']
+FONT_FAMILY = 'Century Gothic'
+
+
+def gen_counter(parent):
+    if LCD_COUNTER:
+        widg = QtWidgets.QLCDNumber(parent)
+        widg.setDigitCount(6)
+        widg.display(0)
+        return widg
+    widg = QtWidgets.QSpinBox(parent)
+    widg.setReadOnly(True)
+    font = QtGui.QFont(FONT_FAMILY)
+    widg.setFont(font)
+    widg.setMinimum(0)
+    widg.setMaximum(int(1e9))
+    widg.setValue(0)
+    widg.setSuffix(MAGNITUDE[0])
+    widg.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+    widg.setGroupSeparatorShown(False)
+    widg.setFrame(False)
+    widg.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+    return widg
 
 
 class SimpleCounterRunningUi(QtWidgets.QMainWindow, Ui_SimpleCounterRunning):
@@ -60,6 +85,10 @@ class SimpleCounterRunningUi(QtWidgets.QMainWindow, Ui_SimpleCounterRunning):
 
         self.show()
 
+    def resizeEvent(self, a0: QtGui.QResizeEvent):
+        self.resize_counters()
+        super().resizeEvent(a0)
+
     def rcv(self, scaler_liste):
         """ handle new incoming data """
         if self.first_call:
@@ -74,12 +103,12 @@ class SimpleCounterRunningUi(QtWidgets.QMainWindow, Ui_SimpleCounterRunning):
                 number_of_new_data_points = scaler_liste[1][i]
                 if number_of_new_data_points:
                     new_data = True
-                    self.elements[i]['widg'].display(last_second_sum)
+                    self.update_counter(i, last_second_sum)
                     self.y_data[i][-1] = last_second_sum
                     self.x_data[i] += number_of_new_data_points * self.sample_interval
                     self.x_data[i][-1] = 0  # always zero at time 0
                     self.update_plot(i, self.x_data[i], self.y_data[i])
-            if self.x_data.shape[1] == self.datapoints:#
+            if self.x_data.shape[1] == self.datapoints:
                 # Once full array size is reached we can proceed with normal data handling below
                 self.arrayFull = True
             elif new_data:
@@ -92,7 +121,7 @@ class SimpleCounterRunningUi(QtWidgets.QMainWindow, Ui_SimpleCounterRunning):
                 last_second_sum = np.sum(j)
                 number_of_new_data_points = scaler_liste[1][i]
                 if number_of_new_data_points:
-                    self.elements[i]['widg'].display(last_second_sum)
+                    self.update_counter(i, last_second_sum)
                     self.y_data[i] = np.roll(self.y_data[i], -1)
                     self.x_data[i] = np.roll(self.x_data[i], -1)
                     self.y_data[i][-1] = last_second_sum
@@ -100,12 +129,27 @@ class SimpleCounterRunningUi(QtWidgets.QMainWindow, Ui_SimpleCounterRunning):
                     self.x_data[i][-1] = 0  # always zero at time 0
                     self.update_plot(i, self.x_data[i], self.y_data[i])
 
+    def update_counter(self, i, last_second_sum):
+        if LCD_COUNTER:
+            self.elements[i]['widg'].display(last_second_sum)
+            return
+        el = self.elements[i]['widg']
+        digits = int(np.floor(np.log10(np.abs(last_second_sum))).astype(int))
+        digits = digits // 3
+        el.setSuffix(MAGNITUDE[digits])
+        val = last_second_sum
+        if digits > 1:
+            val *= 1e-3 ** (digits - 1)
+        el.setValue(int(val + 0.5))
+
     def update_plot(self, indic, xdata, ydata):
         plt_data_item = self.elements[indic].get('plotDataItem', None)
         if plt_data_item is None:
-            self.elements[indic]['plotDataItem'] = Pg.create_plot_data_item(xdata, ydata, pen='b')
+            pen = Pg.create_pen('b', width=2)
+            self.elements[indic]['plotDataItem'] = Pg.create_plot_data_item(xdata, ydata, pen=pen)
             self.elements[indic]['pltItem'].addItem(self.elements[indic]['plotDataItem'])
             self.elements[indic]['pltItem'].vb.invertX(True)  # as requested by Christian
+            self.resize_counters()
         else:
             self.elements[indic]['plotDataItem'].setData(xdata, ydata)
 
@@ -132,7 +176,21 @@ class SimpleCounterRunningUi(QtWidgets.QMainWindow, Ui_SimpleCounterRunning):
     def set_dac_volt(self):
         volt_dbl = self.doubleSpinBox.value()
         Cfg._main_instance.simple_counter_set_dac_volt(volt_dbl)
-        self.label_dac_set_volt.setText(str(volt_dbl))
+
+    def resize_counters(self):
+        if LCD_COUNTER:
+            return
+        font = None
+        scale = 99.
+        for pl_dict in self.elements:
+            el = pl_dict['widg']
+            font = el.font()
+            metric = QtGui.QFontMetrics(font)
+            scale = min([scale, 0.95 * el.width() / metric.horizontalAdvance(el.lineEdit().text())])
+            scale = min([scale, 0.95 * el.height() / metric.height()])
+        font.setPointSize(scale * font.pointSize())
+        for pl_dict in self.elements:
+            pl_dict['widg'].setFont(font)
 
     def splitter_was_moved(self, caller_ind, pos, ind):
         for pl_ind, pl_dict in enumerate(self.elements):
@@ -140,6 +198,7 @@ class SimpleCounterRunningUi(QtWidgets.QMainWindow, Ui_SimpleCounterRunning):
                 pl_dict['splitter'].blockSignals(True)
                 pl_dict['splitter'].moveSplitter(pos, ind)
                 pl_dict['splitter'].blockSignals(False)
+        self.resize_counters()
 
     def add_scalers_to_gridlayout(self, scalers):
         for i, j in enumerate(scalers):
@@ -147,33 +206,32 @@ class SimpleCounterRunningUi(QtWidgets.QMainWindow, Ui_SimpleCounterRunning):
                 name = 'pmt_' + str(j)
                 label_name = 'label_pmt_' + str(j)
                 splitter = QtWidgets.QSplitter(self.centralwidget)
-                splitter.setOrientation(QtCore.Qt.Horizontal)
+                splitter.setOrientation(QtCore.Qt.Orientation.Horizontal)
                 label = QtWidgets.QLabel(splitter)
+                label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+                font = QtGui.QFont(FONT_FAMILY)
+                font.setPointSize(48)
+                label.setFont(font)
                 label.setObjectName(label_name)
-                widg = QtWidgets.QLCDNumber(splitter)
-                widg.setDigitCount(6)
+                label.setText(str(j))
+
+                widg = gen_counter(splitter)
                 sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding,
-                                                   QtWidgets.QSizePolicy.MinimumExpanding)
+                                                   QtWidgets.QSizePolicy.Ignored)
                 sizePolicy.setHorizontalStretch(0)
                 sizePolicy.setVerticalStretch(0)
-                widg.setMinimumWidth(self.width()/5)
+                widg.setMinimumWidth(int(self.width() / 5))
                 sizePolicy.setHeightForWidth(widg.sizePolicy().hasHeightForWidth())
                 widg.setSizePolicy(sizePolicy)
                 widg.setObjectName(name)
 
-                # self.gridLayout_2.addWidget(label, i, 0, 1, 1)
-                # self.gridLayout_2.addWidget(widg, i, 1, 1, 1)
-                plt_widg, plt_item = Pg.create_x_y_widget(x_label='time [s]', y_label='cts')
-                plt_widg.setMinimumWidth(self.width() / 3)
-                splitter.insertWidget(2, plt_widg)
+                plt_widg, plt_item = Pg.create_x_y_widget(x_label='time (s)', y_label='cts')
+                plt_widg.setMinimumWidth(int(self.width() / 3))
+                splitter.insertWidget(1, plt_widg)
+                splitter.setStretchFactor(1, 1)
                 splitter.splitterMoved.connect(functools.partial(self.splitter_was_moved, i))
-                self.gridLayout_2.addWidget(splitter, i, 0, 1, 1)
-                _translate = QtCore.QCoreApplication.translate
-                t = _translate('SimpleCounterRunning',
-                               "<html><head/><body><p><span style=\" font-size:48pt;\">Ch" +
-                               str(j) + "</span></p></body></html>")
-                label.setText(t)
-                widg.display(0)
+                self.centralwidget.layout().insertWidget(i, splitter)
+                self.centralwidget.layout().setStretch(i, 1)
                 sc_dict = {'name': name, 'label': label, 'widg': widg,
                            'plotWid': plt_widg, 'pltItem': plt_item, 'splitter': splitter}
                 self.elements.append(sc_dict)
