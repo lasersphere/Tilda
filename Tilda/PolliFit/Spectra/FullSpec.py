c4bb5d362ff50f445503a62f64d6be79783a3f2e
Patrick Müller <pamueller@ikp.tu-darmstadt.de>
2023-04-14 11:22:40 +0200

    - Fixed an import error in the InteractiveFit.

--- Tilda/PolliFit/Spectra/FullSpec.py
+++ Tilda/PolliFit/Spectra/FullSpec.py
@@ -24,7 +24,7 @@ class FullSpec(object):
         Import the shape and initializes reasonable values 
         """
         # for example: iso.shape['name'] = 'Voigt'
-        shapemod = importlib.import_module('Spectra.' + iso.shape['name'])
+        shapemod = importlib.import_module('Tilda.PolliFit.Spectra.' + iso.shape['name'])
         shape = getattr(shapemod, iso.shape['name'])
         # leading to: Spectra.Voigt.Voigt(iso)
         self.shape = shape(iso)
