- Hotfix:   - LiveDataPlottingUi: Bins are shown at the correct position if there is more than one track, now.   - LiveDataPlottingUi: Fixed a bug that caused the normalization to fail in tracks > 0 after increasing the number of steps.

Tilda/Interface/LiveDataPlottingUi/LiveDataPlottingUi.py
@@ -298,6 +298,7 @@ class TRSLivePlotWindowUi(QtWidgets.QMainWindow, Ui_MainWindow_LiveDataPlotting)
         ''' preset bin/error bar mode for all graphs '''
         self.stepMode = self.actionshow_bins.isChecked()
         self.actionshow_bins.triggered.connect(self.update_show_bins)
+        self.break_points = None
 
         ''' update sum  '''
         self.sum_scaler_changed()
@@ -673,6 +674,11 @@ class TRSLivePlotWindowUi(QtWidgets.QMainWindow, Ui_MainWindow_LiveDataPlotting)
                 st = datetime.now()
                 valid_data = False
                 self.spec_data = deepcopy(spec_data)
+                try:
+                    self.break_points = [_x.size for _x in self.spec_data.x]
+                    self.break_points = [sum(self.break_points[:(i+1)]) for i in range(len(self.break_points))]
+                except Exception as e:
+                    print(e)
 
                 update_time_ms = self.allowed_update_time_ms
                 max_calls_without_plot = 5
@@ -822,7 +828,8 @@ class TRSLivePlotWindowUi(QtWidgets.QMainWindow, Ui_MainWindow_LiveDataPlotting)
                 self.menu_track.setTitle(tr_list[0].text() + '\tCtrl+T  ')
                 self.menu_track.blockSignals(False)
                 self.sum_plt_data = Pg.plot_std(self.sum_x, self.sum_y, self.sum_err, self.sum_plt_itm,
-                                                self.err_sum_plt_item, stepMode=self.stepMode)
+                                                self.err_sum_plt_item, stepMode=self.stepMode,
+                                                break_points=self.break_points)
                 # self.sum_plt_data = self.sum_plt_itm.plot(
                 #     Pg.convert_xaxis_for_step_mode(self.sum_x), self.sum_y, stepMode=True, pen='k')
                 if self.subscribe_as_live_plot:
@@ -831,7 +838,7 @@ class TRSLivePlotWindowUi(QtWidgets.QMainWindow, Ui_MainWindow_LiveDataPlotting)
                     self.sum_plt_itm.addItem(self.sum_current_step_line, ignoreBounds=True)
             else:
                 Pg.set_data_std(self.sum_x, self.sum_y, self.sum_err, self.sum_plt_data, self.err_sum_plt_item,
-                                stepMode=self.stepMode)
+                                stepMode=self.stepMode, break_points=self.break_points)
             self.sum_plt_itm.setLabel('bottom', spec_data.x_units.value)
 
     def update_tres_plot(self, spec_data):
@@ -841,7 +848,7 @@ class TRSLivePlotWindowUi(QtWidgets.QMainWindow, Ui_MainWindow_LiveDataPlotting)
             gates = self.spec_data.softw_gates[self.tres_sel_tr_ind][self.tres_sel_sc_ind]
             # x_range = (float(np.min(spec_data.x[self.tres_sel_tr_ind])), np.max(spec_data.x[self.tres_sel_tr_ind]))
             x_copy = deepcopy(spec_data.x[self.tres_sel_tr_ind])
-            x_copy = Pg.convert_xaxis_for_step_mode(x_copy)
+            x_copy = Pg.convert_xaxis_for_step_mode(x_copy, break_points=None)
             x_range = (x_copy[0], x_copy[-1])
             x_scale = np.mean(np.ediff1d(spec_data.x[self.tres_sel_tr_ind]))
             y_range = (np.min(spec_data.t[self.tres_sel_tr_ind]), np.max(spec_data.t[self.tres_sel_tr_ind]))
@@ -913,13 +920,16 @@ class TRSLivePlotWindowUi(QtWidgets.QMainWindow, Ui_MainWindow_LiveDataPlotting)
                 v_proj_x, v_proj_y, v_proj_err = \
                     spec_data.getArithSpec([self.tres_sel_sc_ind, ], self.tres_sel_tr_ind, eval_on=False)
                 sum_x, sum_y, sum_err = spec_data.getArithSpec(self.sum_scaler, self.tres_sel_tr_ind, self.function)
+                break_points = self.break_points if self.tres_sel_tr_ind == -1 else None
 
                 if self.t_proj_plt is None:
                     self.t_proj_plt = self.t_proj_plt_itm.plot(t_proj_x, t_proj_y, pen='k')
-                    self.sum_proj_plt_data = Pg.plot_std(sum_x, sum_y, sum_err, self.sum_proj_plt_itm,
-                                                         self.err_sum_proj_plt_item, stepMode=self.stepMode)
-                    self.v_proj_plt = Pg.plot_std(v_proj_x, v_proj_y, v_proj_err, self.v_proj_pltitem,
-                                                  self.err_v_proj_plt_item, stepMode=self.stepMode, color='b')
+                    self.sum_proj_plt_data = Pg.plot_std(
+                        sum_x, sum_y, sum_err, self.sum_proj_plt_itm, self.err_sum_proj_plt_item,
+                        stepMode=self.stepMode, break_points=break_points)
+                    self.v_proj_plt = Pg.plot_std(
+                        v_proj_x, v_proj_y, v_proj_err, self.v_proj_pltitem, self.err_v_proj_plt_item,
+                        stepMode=self.stepMode, color='b')
                     self.v_proj_pltitem.vb.addItem(self.v_proj_plt)
 
                     if self.subscribe_as_live_plot:
@@ -938,10 +948,12 @@ class TRSLivePlotWindowUi(QtWidgets.QMainWindow, Ui_MainWindow_LiveDataPlotting)
                     self.t_proj_plt_itm.addItem(self.t_max_line)
                 else:
                     self.t_proj_plt.setData(t_proj_x, t_proj_y)
-                    Pg.set_data_std(self.sum_x, self.sum_y, self.sum_err, self.sum_proj_plt_data,
-                                    self.err_sum_proj_plt_item, stepMode=self.stepMode, color='b')
-                    Pg.set_data_std(v_proj_x, v_proj_y, v_proj_err, self.v_proj_plt,
-                                    self.err_v_proj_plt_item, stepMode=self.stepMode)
+                    Pg.set_data_std(
+                        self.sum_x, self.sum_y, self.sum_err, self.sum_proj_plt_data, self.err_sum_proj_plt_item,
+                        stepMode=self.stepMode, break_points=self.break_points, color='b')
+                    Pg.set_data_std(
+                        v_proj_x, v_proj_y, v_proj_err, self.v_proj_plt, self.err_v_proj_plt_item,
+                        stepMode=self.stepMode)
                     self.v_min_line.setValue(gates[0])
                     self.v_max_line.setValue(gates[1])
                     self.t_min_line.setValue(gates[2])
@@ -970,8 +982,9 @@ class TRSLivePlotWindowUi(QtWidgets.QMainWindow, Ui_MainWindow_LiveDataPlotting)
             self.comboBox_all_pmts_sel_tr.blockSignals(False)
             self.add_all_pmt_plot()
 
+        break_points = self.break_points if self.all_pmts_sel_tr == -1 else None
         Pg.plot_all_sc_new(self.all_pmts_widg_plt_item_list, spec_data, self.all_pmts_sel_tr,
-                           self.function, stepMode=self.stepMode)
+                           self.function, stepMode=self.stepMode, break_points=break_points)
 
         self.all_pmts_widg_plt_item_list[-1]['pltItem'].setLabel('bottom', spec_data.x_units.value)
         self.all_pmts_widg_plt_item_list[0]['pltItem'].setLabel('top', spec_data.x_units.value)

Tilda/PolliFit/PyQtGraphPlotter.py
@@ -51,7 +51,7 @@ def plot_spec_data(spec_data, sc, tr, plot_item=None, pen='k'):
         plot_item.plot(x, y, pen=pen)
 
 
-def plot_std(x, y, err, std_plt, err_plt, stepMode=True, color='k'):
+def plot_std(x, y, err, std_plt, err_plt, stepMode=True, break_points=None, color='k'):
     """
     Create and plot new plotData in a standard pyqtgraph x-y-plot.
 
@@ -61,13 +61,16 @@ def plot_std(x, y, err, std_plt, err_plt, stepMode=True, color='k'):
     :param std_plt: The plot item for the values.
     :param err_plt: The plot item for the uncertainties.
     :param stepMode: Whether to plot bins (without uncertainties) or points with error bars.
+    :param break_points: The points at which to separate the histogram if stepMode.
     :param color: The color of the data.
     :return: None.
     """
     if isinstance(stepMode, bool):
         stepMode = 'center' if stepMode else None
     if stepMode is not None:
-        std_data = std_plt.plot(convert_xaxis_for_step_mode(x), y, stepMode=stepMode, symbol=None, pen=color)
+        std_data = std_plt.plot(convert_xaxis_for_step_mode(x, break_points=break_points),
+                                convert_yaxis_for_step_mode(y, break_points=break_points),
+                                stepMode=stepMode, symbol=None, pen=color)
         err_plt.setData(x=[], y=[], height=None)
     else:
         std_data = std_plt.plot(x, y, stepMode=None, symbol='o', pen=None, symbolBrush=pg.mkBrush(color), symbolSize=5)
@@ -75,7 +78,7 @@ def plot_std(x, y, err, std_plt, err_plt, stepMode=True, color='k'):
     return std_data
 
 
-def set_data_std(x, y, err, std_plt, err_plt, stepMode=True, color='k'):
+def set_data_std(x, y, err, std_plt, err_plt, stepMode=True, break_points=None, color='k'):
     """
     Set new data in a standard pyqtgraph x-y-plot.
 
@@ -85,13 +88,16 @@ def set_data_std(x, y, err, std_plt, err_plt, stepMode=True, color='k'):
     :param std_plt: The plot item for the values.
     :param err_plt: The plot item for the uncertainties.
     :param stepMode: Whether to plot bins (without uncertainties) or points with error bars.
+    :param break_points: The points at which to separate the histogram if stepMode.
     :param color: The color of the data.
     :return: None.
     """
     if isinstance(stepMode, bool):
         stepMode = 'center' if stepMode else None
     if stepMode is not None:
-        std_plt.setData(convert_xaxis_for_step_mode(x), y, stepMode=stepMode, symbol=None, pen=color)
+        std_plt.setData(convert_xaxis_for_step_mode(x, break_points=break_points),
+                        convert_yaxis_for_step_mode(y, break_points=break_points),
+                        stepMode=stepMode, symbol=None, pen=color)
         err_plt.setData(x=[], y=[], height=None)
     else:
         std_plt.setData(x, y, stepMode=None, symbol='o', pen=None, symbolBrush=pg.mkBrush(color), symbolSize=5)
@@ -214,7 +220,7 @@ def create_plot_for_all_sc(target_layout, pmt_list, slot_for_mouse_move, max_rat
     return return_list
 
 
-def plot_all_sc_new(list_of_widgets_etc, spec_data, tr, func, stepMode=True):
+def plot_all_sc_new(list_of_widgets_etc, spec_data, tr, func, stepMode=True, break_points=None):
     """
     create plots in the all pmts tab
     :param list_of_widgets_etc: list of widgest, containig widgets to be plotted
@@ -222,6 +228,7 @@ def plot_all_sc_new(list_of_widgets_etc, spec_data, tr, func, stepMode=True):
     :param tr: list of int, used tracks, -1 for all
     :param func: str, users function
     :param stepMode: Whether to plot bins (without uncertainties) or points with error bars.
+    :param break_points: The points at which to separate the histogram if stepMode.
     """
     for val in list_of_widgets_etc:
         sc = val['indList']  # which scalers are needed for this plot?
@@ -236,14 +243,14 @@ def plot_all_sc_new(list_of_widgets_etc, spec_data, tr, func, stepMode=True):
         # x, y, err = spec_data.calcSpec(func, tr, sc, eval_on)   # calc arithmetic plot
         x, y, err = spec_data.getArithSpec(sc, tr, func, eval_on=eval_on)
 
-        set_data_std(x, y, err, plt_data_itm, plt_err_itm, stepMode=stepMode, color=color)
+        set_data_std(x, y, err, plt_data_itm, plt_err_itm, stepMode=stepMode, break_points=break_points, color=color)
 
         # if stepMode:
         #     x = convert_xaxis_for_step_mode(deepcopy(x))
         # plt_data_itm.setData(x, y, stepMode=stepMode)
 
 
-def plot_all_sc(list_of_widgets_etc, spec_data, tr, stepMode=True):
+def plot_all_sc(list_of_widgets_etc, spec_data, tr, stepMode=True, break_points=None):
     # print('plotting all pmts in %s' % list_of_widgets_etc)
     for val in list_of_widgets_etc:
         sc = val['indList']
@@ -252,18 +259,38 @@ def plot_all_sc(list_of_widgets_etc, spec_data, tr, stepMode=True):
         # x, y, err = spec_data.getArithSpec(sc, tr)
         if isinstance(stepMode, bool):
             if stepMode:
-                x = convert_xaxis_for_step_mode(deepcopy(x))
+                x = convert_xaxis_for_step_mode(x, break_points=break_points)
+                y = convert_yaxis_for_step_mode(y, break_points=break_points)
                 stepMode = 'center'
             else:
                 stepMode = None
         plt_data_itm.setData(x, y, stepMode=stepMode)
 
 
-def convert_xaxis_for_step_mode(x_axis):
-    x_axis_step = np.mean(np.ediff1d(x_axis))
-    x_axis = np.append(x_axis, [x_axis[-1] + x_axis_step])
-    x_axis += -0.5 * abs(x_axis_step)
-    return x_axis
+def convert_xaxis_for_step_mode(x_axis, break_points=None):
+    # Do not use the legacy function for more than a single track.
+    if break_points is None:
+        x_axis_step = np.mean(np.ediff1d(x_axis))
+        x_axis = np.append(x_axis, [x_axis[-1] + x_axis_step])
+        x_axis += -0.5 * abs(x_axis_step)
+        return x_axis
+    i = 0
+    x_axes = []
+    for j in break_points:
+        _x_axis = x_axis[slice(i, j, 1)]
+        _x_axis_step = np.mean(np.ediff1d(_x_axis))
+        _x_axis = np.append(_x_axis, [_x_axis[-1] + _x_axis_step])
+        _x_axis += -0.5 * abs(_x_axis_step)
+        x_axes.append(_x_axis)
+        i = j
+    return np.concatenate(x_axes, axis=0)
+
+
+def convert_yaxis_for_step_mode(y_axis, break_points=None):
+    # Do not use the legacy function for more than a single track.
+    if break_points is None or len(break_points) < 2:
+        return y_axis
+    return np.insert(y_axis, break_points[:-1], np.min(y_axis))
 
 
 def create_viewbox():