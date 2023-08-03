6d3b6534a3b6c7182ae2dccbf7fd4cfe6f54a09f
EdwardMatth <edwardmath@live.com>
2023-08-03 16:34:53 +0200

    implemented MQTT to change DAC voltage using the simplecounter as a guide, duplicated simple counter to achieve this

--- Tilda/Interface/MQTTSimpleCounter/SimpleCounterDialogUi.py
+++ Tilda/Interface/MQTTSimpleCounter/SimpleCounterDialogUi.py
@@ -29,7 +29,7 @@ class SimpleCounterDialogUi(QtWidgets.QDialog, Ui_Dialog_simpleCounterControl):
         self.spinBox_plotpoints.valueChanged.connect(self.set_datapoints)
 
         ''' default vals: '''
-        self.lineEdit_act_pmts.setText('0, 1, 2, 3')
+        self.lineEdit_act_pmts.setText('')
         self.spinBox_plotpoints.setValue(600)
 
         self.exec_()
